10a00e3886c2cfceaa3a5dd567d01e04cd7cee2f
Christoph sticksel <christoph-sticksel@uiowa.edu>
2013-10-03 12:52:04 -0500

    Fixed type declaration

--- src/oldParser.mli
+++ src/oldParser.mli
@@ -38,7 +38,7 @@ This file is part of the Kind verifier
 
 
 (** Convert a Kind-1 IL expression to a term *)
-val il_expression_to_term : bool -> Kind1.Types.il_formula -> Term.t
+val il_expression_to_term : bool -> Kind1.Types.lustre_type option * Kind1.Types.il_expression -> Term.t
 
 (** Convert a Kind-1 IL formula to a term *)
 val il_formula_to_term : bool -> Kind1.Types.il_formula -> Term.t
