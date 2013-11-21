a9302e6ef5fab442a18232605f1cc993f768fc32
Christoph sticksel <christoph-sticksel@uiowa.edu>
2013-11-20 19:19:47 -0600

    Fixed handling of assertions

--- src/oldParser.ml
+++ src/oldParser.ml
@@ -1374,10 +1374,12 @@ let of_channel in_ch =
      
 
      (* Convert internal representation to assignments in initial state *)
-     let _, init_assignments = assignments_of_il_formulas true [] [] [fd'] in
+     let invariants, init_assignments = 
+       assignments_of_il_formulas true [] [] [fd'] 
+     in
      
      (* Convert internal representation to assignments in transition *)
-     let invariants, trans_assignments = 
+     let _, trans_assignments = 
        assignments_of_il_formulas false [] [] [fd'] 
      in
 
