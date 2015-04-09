a0bc7b55bb5282bfe9fc60f5e9a538e5789eadd5
Alain Mebsout <alainmebsout@gmail.com>
2015-04-09 15:59:42 -0500

    Actual identity detection for lambdas

--- src/term.ml
+++ src/term.ml
@@ -768,19 +768,20 @@ let import = T.import
 (* Import a term from a different instance into this hashcons table *)
 let import_lambda = T.import_lambda 
 
+(* Flatten top node of term *)
+let construct = T.construct
 
 (* Returns true if the lamda expression is the identity, i.e. lambda x.x *)
 let is_lambda_identity l =
   try
     let v = Var.mk_fresh_var Type.t_bool in
     let tv = mk_var v in
-    let lv = eval_lambda l [tv] in
+    let lv = eval_lambda l [tv]
+             |> eval_t (fun t _ -> t)
+             |> construct
+    in
     equal tv lv
   with Invalid_argument _ -> false
-
-
-(* Flatten top node of term *)
-let construct = T.construct
 
 
 (* Is the term a Boolean atom? *)
