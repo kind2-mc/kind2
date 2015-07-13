f7aa6a848f329b25f2d31e068a7bcf4fd2073741
Christoph sticksel <christoph-sticksel@uiowa.edu>
2015-07-13 15:21:51 -0500

    Removed debug output

--- src/lustre/lustreTransSys.ml
+++ src/lustre/lustreTransSys.ml
@@ -1893,9 +1893,9 @@ let rec trans_sys_of_node'
                 [] (* One-state invariants *)
                 [] (* Two-state invariants *)
             in                
-
+(*
             Format.printf "%a@." TransSys.pp_print_trans_sys trans_sys;
-  
+  *)
             trans_sys_of_node'
               top_name
               analysis_param
@@ -1936,11 +1936,11 @@ let trans_sys_of_nodes
   in
 
   let nodes = N.nodes_of_subsystem subsystem' in 
-
+(*
   Format.printf
     "@[<v>%a@]@."
     (pp_print_list (N.pp_print_node false) "@,") (List.rev nodes);
-
+*)
   let { trans_sys } =   
 
     try 
