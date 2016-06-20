343650c54b70c592ba3ca5fc52b6d09f98350cb3
Adrien Champion <adrien.champion@email.com>
2016-06-20 10:06:16 -0500

    Fixed 4.03 warnings

--- src/utils/hString.ml
+++ src/utils/hString.ml
@@ -224,16 +224,16 @@ let contains_from { Hashcons.node = n } i c = Bytes.contains_from n i c
 let rcontains_from { Hashcons.node = n } i c = Bytes.rcontains_from n i c
 
 let uppercase { Hashcons.node = n } =
-  mk_hstring (Bytes.uppercase n)
+  mk_hstring (Bytes.uppercase_ascii n)
 
 let lowercase { Hashcons.node = n } =
-  mk_hstring (Bytes.lowercase n)
+  mk_hstring (Bytes.lowercase_ascii n)
 
 let capitalize { Hashcons.node = n } =
-  mk_hstring (Bytes.capitalize n)
+  mk_hstring (Bytes.capitalize_ascii n)
 
 let uncapitalize { Hashcons.node = n } =
-  mk_hstring (Bytes.uncapitalize n)
+  mk_hstring (Bytes.uncapitalize_ascii n)
 
 
 (* 
