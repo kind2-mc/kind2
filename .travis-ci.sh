3c48fcd72c09fd014a1c4fb6324916afc179249a
Adrien Champion <adrien.champion@email.com>
2016-12-13 15:31:26 +0100

    Travis stuff

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,16 +2,25 @@
 OPAM_DEPENDS="ocamlfind menhir camlp4"
 
 sudo apt-get update -y -qq
-sudo apt-get install -y -qq ocaml ocaml-native-compilers
 
+# Install z3.
+git clone https://github.com/Z3Prover/z3
+cd z3
+./configure
+cd build
+make
+sudo make install
+cd ../../
+
+sudo apt-get install -y -qq ocaml ocaml-native-compilers
 
 # Retrieve opam.
 wget -qq https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
 export OPAMYES=1
 opam init
 eval $(opam config env)
-opam update
 opam switch 4.04.0
+opam update
 
 # Install ocaml packages needed for Kind 2
 opam install ocamlfind camlp4 menhir
