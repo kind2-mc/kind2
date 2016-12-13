aec237ee6882ed7c39cf8d2a20163bc11be62d6c
Adrien Champion <adrien.champion@email.com>
2016-12-13 14:47:43 +0100

    Travis stuff

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,16 +1,22 @@
 # Kind 2 dependencies.
 OPAM_DEPENDS="ocamlfind menhir camlp4"
 
-ppa=avsm/ocaml44+opam12
-   
-sudo add-apt-repository ppa:$ppa
 sudo apt-get update -y -qq
-sudo apt-get install -y -qq ocaml ocaml-native-compilers camlp4-extra opam
-export OPAMYES=1
-opam init
-opam switch "$OCAML_VERSION"
-opam install ${OPAM_DEPENDS}
-eval `opam config env`
+sudo apt-get install -y -qq ocaml ocaml-native-compilers
+
+
+# Retrieve opam.
+wget -qq https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
+echo "y" | opam init
+eval $(opam config env)
+opam update
+opam switch 4.04.0
+
+# Install ocaml packages needed for Kind 2
+echo "y" | opam install ocamlfind camlp4 menhir
+echo "y" | opam init && eval $(opam config env) && opam switch reinstall system
+eval $(opam config env)
+
 ./autogen.sh
 ./build.sh
 make test
