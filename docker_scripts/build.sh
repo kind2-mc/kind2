0094867c6098a6d6a5e3822e5512215261578d9e
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2018-09-01 10:51:08 -0500

    Updated CVC4 version used in Docker, and retrieved Yices 2 binary

--- docker_scripts/build.sh
+++ docker_scripts/build.sh
@@ -30,13 +30,19 @@ unzip z3-4.7.1-x64-ubuntu-16.04.zip
 mv z3-4.7.1-x64-ubuntu-16.04/bin/z3 bin/.
 rm -rf z3-4.7.1-x64-ubuntu-16.04.zip z3-4.7.1-x64-ubuntu-16.04/
 
+# Retrieve Yices 2
+wget -qq http://yices.csl.sri.com/releases/2.6.0/yices-2.6.0-x86_64-pc-linux-gnu-static-gmp.tar.gz
+tar xvf yices-2.6.0-x86_64-pc-linux-gnu-static-gmp.tar.gz
+mv yices-2.6.0/bin/yices-smt2 bin/
+rm -rf yices-2.6.0-x86_64-pc-linux-gnu-static-gmp.tar.gz yices-2.6.0
+
 # Retrieve JKind and CVC4 (required for certification)
 wget -qq https://github.com/agacek/jkind/releases/download/v4.0.1/jkind.zip
 unzip jkind.zip && mv jkind/jkind jkind/*.jar bin/ && chmod a+x bin/jkind*
 rm -rf jkind.zip jkind
 
-wget -qq http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/unstable/cvc4-2018-08-15-x86_64-linux-opt
-mv cvc4-2018-08-15-x86_64-linux-opt bin/cvc4 && chmod a+x bin/cvc4
+wget -qq http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/unstable/cvc4-2018-08-31-x86_64-linux-opt
+mv cvc4-2018-08-31-x86_64-linux-opt bin/cvc4 && chmod a+x bin/cvc4
 
 # Remove packages not required anymore
 apt-get purge -y automake camlp4 libtool menhir pkg-config ocaml ocamlbuild unzip wget
