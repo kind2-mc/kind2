2efc270efa955084af372b461e03e41fe93cadd3
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2018-10-20 15:17:34 -0500

    Updated Docker build script.

--- docker_scripts/build.sh
+++ docker_scripts/build.sh
@@ -19,30 +19,33 @@ cd kind2-build
 ./autogen.sh
 ./build.sh
 
-# Copy Kind 2 binary and remove build directory
+# Copy Kind 2 binary
 mv bin/kind2 ../
 cd ..
-rm -rf kind2-build
 
 # Retrieve Z3
 wget -qq https://github.com/Z3Prover/z3/releases/download/z3-4.7.1/z3-4.7.1-x64-ubuntu-16.04.zip
 unzip z3-4.7.1-x64-ubuntu-16.04.zip
-mv z3-4.7.1-x64-ubuntu-16.04/bin/z3 bin/.
+mv z3-4.7.1-x64-ubuntu-16.04/bin/z3 ./
 rm -rf z3-4.7.1-x64-ubuntu-16.04.zip z3-4.7.1-x64-ubuntu-16.04/
 
 # Retrieve Yices 2
 wget -qq http://yices.csl.sri.com/releases/2.6.0/yices-2.6.0-x86_64-pc-linux-gnu-static-gmp.tar.gz
 tar xvf yices-2.6.0-x86_64-pc-linux-gnu-static-gmp.tar.gz
-mv yices-2.6.0/bin/yices-smt2 bin/
+mv yices-2.6.0/bin/yices-smt2 ./
 rm -rf yices-2.6.0-x86_64-pc-linux-gnu-static-gmp.tar.gz yices-2.6.0
 
 # Retrieve JKind and CVC4 (required for certification)
 wget -qq https://github.com/agacek/jkind/releases/download/v4.0.1/jkind.zip
-unzip jkind.zip && mv jkind/jkind jkind/*.jar bin/ && chmod a+x bin/jkind*
-rm -rf jkind.zip jkind
+unzip jkind.zip && mv jkind jkind_zip && mv jkind_zip/jkind jkind_zip/*.jar ./
+rm -rf jkind.zip jkind_zip
+chmod a+x ./jkind*
 
-wget -qq http://cvc4.cs.stanford.edu/downloads/builds/x86_64-linux-opt/unstable/cvc4-2018-08-31-x86_64-linux-opt
-mv cvc4-2018-08-31-x86_64-linux-opt bin/cvc4 && chmod a+x bin/cvc4
+./kind2-build/docker_scripts/latest_cvc4.sh
+chmod a+x ./cvc4
+
+# Remove Kind 2 build directory
+rm -rf kind2-build
 
 # Remove packages not required anymore
 apt-get purge -y automake camlp4 libtool menhir pkg-config ocaml ocamlbuild unzip wget
