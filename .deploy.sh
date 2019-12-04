2d863bc7d420c3ae17290b13e965cf7572af627c
Kevin Blicharski <blicharskikevin@gmail.com>
2019-12-04 05:55:09 -0600

    differentiate builds for osx and linux

--- .deploy.sh
+++ .deploy.sh
@@ -1,12 +1,13 @@
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+if [[ -f bin/kind2 ]]; then 
+  mv bin/kind2 bin/kind2-$TRAVIS_TAG-$TRAVIS_OS_NAME
+fi
 
 openssl aes-256-cbc -k "$TRAVIS_KEY_PASSWORD" -d -md sha256 -a -in travis_key.enc -out travis_key
 echo "Host github.com" > ~/.ssh/config
 echo "  IdentityFile  $(pwd)/travis_key" >> ~/.ssh/config
 chmod 400 travis_key
-git remote set-url origin "git@github.com:${TRAVIS_REPO_SLUG}.git"
+git remote set-url origin "git@github.com:$TRAVIS_REPO_SLUG.git"
 echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > ~/.ssh/known_hosts 
 
-git tag -f "nightly-${TRAVIS_OS_NAME}"
+git tag -f nightly
 git push --tags -f
