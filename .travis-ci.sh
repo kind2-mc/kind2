4e9c64d7133086c3c1ce6588ad00826397d007fd
Kevin Blicharski <blicharskikevin@gmail.com>
2019-12-04 00:39:20 -0600

    typo fix; temp disable branches 'only' for testing

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,14 +1,14 @@
 
 # Install z3.
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-  z3_version="z3-4.7.1/z3-4.7.1-x64-ubuntu-14.04"
+  z3_version="z3-4.7.1-x64-ubuntu-14.04"
   install_dir="/usr/bin/z3"
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then 
-  z3_version="z3-4.7.1/z3-4.7.1-x64-osx-10.11.6"
+  z3_version="z3-4.7.1-x64-osx-10.11.6"
   install_dir="/usr/local/bin/z3"
 fi
 
-wget "https://github.com/Z3Prover/z3/releases/download/${z3_version}.zip"
+wget "https://github.com/Z3Prover/z3/releases/download/z3-4.7.1/${z3_version}.zip"
 unzip "${z3_version}.zip"
 sudo cp "${z3_version}/bin/z3" $install_dir
 
