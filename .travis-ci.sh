611f4fa189e183ce8b9c079fbd2b3000130f6981
Kevin Blicharski <blicharskikevin@gmail.com>
2019-12-03 23:20:15 -0600

    attempt to fix installation of z3 on osx

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,8 +1,14 @@
 
 # Install z3.
-wget https://github.com/Z3Prover/z3/releases/download/z3-4.7.1/z3-4.7.1-x64-ubuntu-14.04.zip
-unzip z3-4.7.1-x64-ubuntu-14.04.zip
-sudo cp z3-4.7.1-x64-ubuntu-14.04/bin/z3 /usr/bin/z3
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+  wget https://github.com/Z3Prover/z3/releases/download/z3-4.7.1/z3-4.7.1-x64-ubuntu-14.04.zip
+  unzip z3-4.7.1-x64-ubuntu-14.04.zip
+  sudo cp z3-4.7.1-x64-ubuntu-14.04/bin/z3 /usr/bin/z3
+elif [ "$TRAVIS_OS_NAME" = "osx" ]; then 
+  wget https://github.com/Z3Prover/z3/releases/download/z3-4.7.1/z3-4.7.1-x64-osx-10.11.6.zip
+  unzip z3-4.7.1-x64-osx-10.11.6.zip
+  sudo cp z3-4.7.1-x64-osx-10.11.6.zip /usr/local/bin/z3
+fi
 
 # Retrieve opam.
 wget -qq https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin 4.04.0
