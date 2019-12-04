aa2ff34ecabb13dd8cdf167fdca30fc42044526f
Kevin Blicharski <blicharskikevin@gmail.com>
2019-12-04 00:34:24 -0600

    clean up z3 install; new deploy script; new job matrix

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,14 +1,16 @@
 
 # Install z3.
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-  wget https://github.com/Z3Prover/z3/releases/download/z3-4.7.1/z3-4.7.1-x64-ubuntu-14.04.zip
-  unzip z3-4.7.1-x64-ubuntu-14.04.zip
-  sudo cp z3-4.7.1-x64-ubuntu-14.04/bin/z3 /usr/bin/z3
+  z3_version="z3-4.7.1/z3-4.7.1-x64-ubuntu-14.04"
+  install_dir="/usr/bin/z3"
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then 
-  wget https://github.com/Z3Prover/z3/releases/download/z3-4.7.1/z3-4.7.1-x64-osx-10.11.6.zip
-  unzip z3-4.7.1-x64-osx-10.11.6.zip
-  sudo cp z3-4.7.1-x64-osx-10.11.6.zip /usr/local/bin/z3
+  z3_version="z3-4.7.1/z3-4.7.1-x64-osx-10.11.6"
+  install_dir="/usr/local/bin/z3"
 fi
+
+wget "https://github.com/Z3Prover/z3/releases/download/${z3_version}.zip"
+unzip "${z3_version}.zip"
+sudo cp "${z3_version}/bin/z3" $install_dir
 
 # Retrieve opam.
 wget -qq https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin 4.04.0
