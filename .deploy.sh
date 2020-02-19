4df7ba9b9317e6b1ee33ae52a0126acd1f857b89
Kevin Blicharski <blicharskikevin@gmail.com>
2020-02-19 05:38:04 -0600

    Testing OSX issues still

--- .deploy.sh
+++ .deploy.sh
@@ -28,16 +28,17 @@ git tag -f nightly
 git push --tags -f
 
 # OSX doesn't ship with a version of Python 3 on Travis
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then 
-  brew install python
-  python --version
-  python3 --version
-fi
 
 
 # Clear all older uploaded release artifacts for the `nightly` tag
 # `pyenv global 3.6` is required to instruct Travis to use Python 3 rather than 2
 # `$DATE_STRING` is passed so the Mac OS build doesn't overwrite the Linux binary
-pyenv global 3.6
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then 
+    pyenv global 3.6
+fi
+pip --version
+python --version
+pip3 --version
+python3 --version
 pip install --user requests
 python scripts/travis-clean-nightly.py $DATE_STRING
