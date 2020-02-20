98e57e1b988239bd82b12e045611dca7bc38804d
Kevin Blicharski <blicharskikevin@gmail.com>
2020-02-20 00:40:51 -0600

    Add auth headers and fix OSX bug

--- scripts/travis-clean-nightly.py
+++ scripts/travis-clean-nightly.py
@@ -27,12 +27,10 @@ def assets_present():
 # Wrap in a loop to take care of intermittent API failures
 i = 0
 while release.get('id') is None and i < TIMEOUT:
-    release = get('{}/releases/tags/nightly'.format(BASE_URL)).json()
+    release = get('{}/releases/tags/nightly'.format(BASE_URL), headers=AUTH_HEADER).json()
     time.sleep(1)
     i += 1
 
-# Should not be empty by this point
-assert release
 print('Took {} attempts to fetch the release...'.format(i))
 pprint(release)
 
@@ -40,21 +38,21 @@ pprint(release)
 # Get all of the assets for that release
 i = 0
 while not assets_present() and i < TIMEOUT:
-    assets = get('{}/releases/{}/assets'.format(BASE_URL, release['id'])).json()
+    assets = get('{}/releases/{}/assets'.format(BASE_URL, release['id']), headers=AUTH_HEADER).json()
     time.sleep(1)
     i += 1
 
-# Should not be empty by this point
-assert assets
 print('Took {} attempts to fetch release assets...'.format(i))
 pprint(assets)
 
-# Map each asset to its ID and filter out those with the current date,
-# as these should either be kept (so the OSX build doesn't delete the Linux build's asset)
-# or overwritten (in the case that cron runs and releases a new asset on the same day)
-asset_ids = [asset['id'] for asset in assets if TIMESTAMP not in asset['name']]
+# The OSX build may not find any assets if the Linux build had deleted them
+if assets:
+    # Map each asset to its ID and filter out those with the current date,
+    # as these should either be kept (so the OSX build doesn't delete the Linux build's asset)
+    # or overwritten (in the case that cron runs and releases a new asset on the same day)
+    asset_ids = [asset['id'] for asset in assets if TIMESTAMP not in asset['name']]
 
-# Delete each asset individually by ID
-for asset_id in asset_ids:
-    delete('{}/releases/assets/{}'.format(BASE_URL, asset_id), headers=AUTH_HEADER)
-    time.sleep(1)
+    # Delete each asset individually by ID
+    for asset_id in asset_ids:
+        delete('{}/releases/assets/{}'.format(BASE_URL, asset_id), headers=AUTH_HEADER)
+        time.sleep(1)
