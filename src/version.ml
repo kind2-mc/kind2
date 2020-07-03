7037ad5f055b32f77383897d3b8d409c404f484a
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2020-07-03 08:56:56 -0500

    Use dune build info

--- src/version.ml
+++ src/version.ml
@@ -1,3 +1,6 @@
 let package_name = "kind2"
 
-let version = "%%VERSION%%"
+let version =
+  (match Build_info.V1.version () with
+   | None -> "<version>"
+   | Some v -> Build_info.V1.Version.to_string v)
