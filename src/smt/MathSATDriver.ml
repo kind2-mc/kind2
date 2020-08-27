0b4493c39f2cd653fd1ff00dcdda8cc95a7c80c6
M. Fareed Arif <fareed.arif@yahoo.com>
2020-08-27 12:27:14 -0500

    added aruments to specific to BV

--- src/smt/MathSATDriver.ml
+++ src/smt/MathSATDriver.ml
@@ -37,7 +37,10 @@ let cmd_line logic timeout produce_assignments produce_proofs produce_cores
   in
   let timeout = Lib.min_option timeout_global timeout_local in
 
-  let base_cmd = [| mathsat_bin; "-shallow_incrementality=true" ; "-allow_bool_function_args=true" |] in
+  let base_cmd = [| mathsat_bin; "-shallow_incrementality=true" ;
+                                 "-preprocessor.simplification=2"; 
+                                 "-allow_bool_function_args=true" ;
+                                 "-theory.bv.eager=true"   |] in
   match timeout with
   | None -> base_cmd
   | Some timeout ->
