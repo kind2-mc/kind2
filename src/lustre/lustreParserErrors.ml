3ae7a7345e92c55523fe4ad7135b3408991eb6ee
Apoorv Ingle <apoorv.ingle@gmail.com>
2020-09-22 13:59:35 -0500

    some more error messages

--- src/lustre/lustreParserErrors.ml
+++ src/lustre/lustreParserErrors.ml
@@ -81,7 +81,7 @@ let message =
     | 461 ->
         "Syntax Error!\n"
     | 822 ->
-        "Syntax Error!\n"
+        "Syntax Error! List of expressions cannot be on the left hand side of the equation. \n"
     | 875 ->
         "Syntax Error! Unexpected parenthesis found.\nArray indexing is done using square brackets e.g. `A[i]`.\n"
     | 940 ->
