4104ef2688ea1da1955c5ab46135eca80fdc8862
Abdalrhman M Mohamed <abdoo8080@outlook.com>
2020-12-27 21:57:50 -0600

    Fix typos.
    Signed-off-by: Abdalrhman M Mohamed <abdoo8080@outlook.com>

--- src/lustre/lspInfo.ml
+++ src/lustre/lspInfo.ml
@@ -1,15 +1,14 @@
 let lsp_component_json pos id =
   let file, lnum, cnum = Lib.file_row_col_of_pos pos in
   Format.fprintf !Lib.log_ppf
-    "{@[<v 1>@,\
-     \"objectType\" : \"log\",@,\
+    ",@.{@[<v 1>@,\
+     \"objectType\" : \"lsp\",@,\
      \"source\" : \"lsp\",@,\
      \"name\" : \"%s\",@,\
      \"file\" : \"%s\",@,\
      \"line\" : %d,@,\
-     \"column\" : %d,@,\
-     @]@,\
-     }"
+     \"column\" : %d\
+    }"
     id file lnum cnum
 
 let print_ast_info declarations =
