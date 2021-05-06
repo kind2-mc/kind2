kind2-mc/kind2

commit a843ffa1ae29935a4a975c8893e5e8f9598c47af

Declare SoFar(A) invariant when A=A_1..A_n has been proven invariant

src/kEvent.ml
@@ -585,7 +585,7 @@ let prop_attributes_xml trans_sys prop_name =
     | Property.Candidate (Some source) -> get_attributes source
     | Property.Instantiated (scope,prop) ->
         get_attributes prop.Property.prop_source
-    | Property.Assumption (pos, scope) ->
+    | Property.Assumption (pos, (scope, _)) ->
         let fname, lnum, cnum = file_row_col_of_pos pos in
         Format.asprintf " line=\"%d\" column=\"%d\" scope=\"%s\" source=\"Assumption\"%a"
           lnum cnum (String.concat "." scope) pp_print_fname fname
@@ -909,7 +909,7 @@ let prop_attributes_json ppf trans_sys prop_name =
           pp_print_fname fname lnum cnum
     | Property.Instantiated (scope,prop) ->
         get_attributes prop.Property.prop_source
-    | Property.Assumption (pos, scope) -> print_attributes pos scope "Assumption"
+    | Property.Assumption (pos, (scope, _)) -> print_attributes pos scope "Assumption"
     | Property.Guarantee (pos, scope) -> print_attributes pos scope "Guarantee"
     | Property.GuaranteeOneModeActive (pos, scope) -> print_attributes pos scope "OneModeActive"
     | Property.GuaranteeModeImplication (pos, scope) -> print_attributes pos scope "Ensure"
@@ -1708,6 +1708,61 @@ let invariant scope term cert two_state =
   with Messaging.NotInitialized -> ()
 
 
+let check_sofar_invariance trans_sys pos =
+  match TransSys.get_sofar_term trans_sys pos with
+  | Some (sofar_term, is_invariant) when is_invariant -> (
+    let cert = (1, sofar_term) in
+    (* Add invariant to transtion system *)
+    Some (TransSys.add_invariant trans_sys sofar_term cert false)
+  )
+  | _ -> None
+
+
+(* Broadcast a property is invariant, and infer new invariants *)
+let prop_invariant trans_sys prop_name cert =
+
+  (* Update time in case we are not running in parallel mode *)
+  Stat.update_time Stat.total_time ;
+  Stat.update_time Stat.analysis_time ;
+
+  let mdl = get_module () in
+
+  log_proved mdl L_warn trans_sys None prop_name ;
+
+  (* Get property by name *)
+  let prop = TransSys.property_of_name trans_sys prop_name in
+  let status = Property.PropInvariant cert in
+
+  (* Update status of property in transition system *)
+  Property.set_prop_status prop status;
+
+  (* Add property as invariant to transtion system *)
+  TransSys.add_invariant trans_sys prop.prop_term cert false |> ignore ;
+
+  (try
+
+    (* Send status message *)
+    EventMessaging.send_relay_message (PropStatus (prop_name, status))
+
+  (* Don't fail if not initialized *)
+  with Messaging.NotInitialized -> () ) ;
+
+  match prop.Property.prop_source with
+  | Property.Assumption (_, (_, pos)) -> (
+    (* If property was proven invariant and it is a contract assumption
+       that a caller had to prove, check whether the other assumptions
+       have been proved invariant too. If so, set SoFar(assumptions)
+       invariant in transition system.
+
+       There is a similar check in [update_trans_sys_sub] that updates
+       the transition system in the other processes.
+    *)
+    match check_sofar_invariance trans_sys pos with
+    | None -> TSet.empty
+    | Some inv -> TSet.singleton inv
+  )
+  | _ -> TSet.empty
+
 
 (* Broadcast a property status *)
 let prop_status status input_sys analysis trans_sys prop =
@@ -2028,9 +2083,11 @@ let update_trans_sys_sub input_sys analysis trans_sys events =
 
       (* Output proved property *)
       log_proved m L_warn trans_sys None p;
-          
-      (* Change property status in transition system *)
-      TransSys.set_prop_invariant trans_sys p cert;
+
+      (* Get property by name *)
+      let prop = TransSys.property_of_name trans_sys p in
+      (* Change property status (in transition system) *)
+      Property.set_prop_status prop s ;
 
       let term =
         TransSys.props_list_of_bound trans_sys Numeral.zero
@@ -2046,6 +2103,20 @@ let update_trans_sys_sub input_sys analysis trans_sys events =
           |> insert_inv scope invars false
         with Not_found -> (* Skip if named property not found *)
           invars
+      in
+
+      let invars =
+        match prop.Property.prop_source with
+        | Property.Assumption (_, (_, pos)) -> (
+          (* If property is a contract assumption that a caller had to prove,
+             check whether the other assumptions have been proved invariant too.
+             If so, set SoFar(assumptions) invariant in transition system.
+          *)
+          match check_sofar_invariance trans_sys pos with
+          | Some inv -> insert_inv scope invars false inv
+          | None -> invars
+        )
+        | _ -> invars
       in
 
       (* Continue with property status added to accumulator *)

src/certif/certifChecker.ml
@@ -2408,7 +2408,8 @@ let mk_inst init_flag sys formal_vars =
   [ { TransSys.pos = Lib.dummy_pos;
       map_down;
       map_up;
-      guard_clock = fun _ t -> t } ]
+      guard_clock = (fun _ t -> t);
+      assumes = None } ]
 
 
 let add_scope_and_register_bounds scope orig_tbl dest_tbl sv =

src/induction/step.ml
@@ -476,15 +476,18 @@ let rec next input_sys aparam trans solver k unfalsifiables unknowns =
     )
   in
   
-  (* Communicating confirmed properties. *)
-  confirmed_cert
-  |> List.iter (
-    fun (s, (_, _, cert)) ->
-      KEvent.prop_status
-        (Property.PropInvariant cert) input_sys aparam trans s ;
-      (* KEvent.log L_warn
+  (* Communicating confirmed properties,
+     and getting new inferred invariants *)
+  let new_invs =
+    List.fold_left
+      (fun acc (s, (_, _, cert)) ->
+        (* KEvent.log L_warn
         "%s: @[<v>%d, %a@]" s (fst cert) Term.pp_print_term (snd cert) ; *)
-  ) ;
+        KEvent.prop_invariant trans s cert |> Term.TermSet.union acc
+      )
+      (fst new_invs)
+      confirmed_cert, (snd new_invs)
+  in
 
   match unknowns', unfalsifiables with
   | [], [] ->

src/induction/step2.ml
@@ -301,11 +301,20 @@ let broadcast_if_safe ({ solver ; sys ; map } as ctx) unfalsifiable =
         false
     )
     | [] ->
-      (* All properties confirmed, broadcasting as invariant. *)
-      confirmed |> List.iter (
-        fun (prop, cert) ->
-          KEvent.prop_status (Prop.PropInvariant cert)
-            ctx.in_sys ctx.param sys prop
+      (* All properties confirmed, broadcasting as invariant,
+         and getting new inferred invariants
+      *)
+      let new_os_invs =
+        confirmed |> List.fold_left (
+          fun acc (prop, cert) ->
+            KEvent.prop_invariant sys prop cert |> Term.TermSet.union acc
+        )
+        Term.TermSet.empty
+      in
+      if not (Term.TermSet.is_empty new_os_invs) then (
+        let new_invariants = (new_os_invs, Term.TermSet.empty) in
+        (* Forcing to assert invs to [3], since upper-bound's exclusive. *)
+        Unroller.assert_new_invs_to solver Num.(succ two) new_invariants
       ) ;
       (* Removing from map and updating solver. *)
       let map =

src/kEvent.mli
@@ -186,6 +186,11 @@ val step_cex :
 (** Broadcast a property status *)
 val prop_status : Property.prop_status -> 'a InputSystem.t -> Analysis.param -> TransSys.t -> string -> unit
 
+(** Broadcast a property is invariant, and return a set of
+    logical consequences that are also invariant
+*)
+val prop_invariant : TransSys.t -> string -> Certificate.t -> Term.TermSet.t
+
 (* Log a property disproved during the computation of a Minimal Cut Set *)
 val cex_wam : (StateVar.t * Model.value list) list -> (string * bool) list -> 'a InputSystem.t -> Analysis.param -> TransSys.t -> string -> unit
 

src/lustre/lustreTransSys.ml
@@ -287,7 +287,7 @@ let subrequirements_of_contract call_pos scope svar_map { C.assumes } =
         )
       in
       let prop_status = P.PropUnknown in
-      let prop_source = P.Assumption (pos, scope) in
+      let prop_source = P.Assumption (pos, (scope, call_pos)) in
       { P.prop_name ;
         P.prop_source ;
         P.prop_term ;
@@ -473,13 +473,15 @@ let add_subsystem
     map_up
     map_down
     guard_clock
+    assumes
     subsystems =
 
   let instance =
     { TransSys.pos; 
       TransSys.map_up; 
       TransSys.map_down; 
-      TransSys.guard_clock }
+      TransSys.guard_clock;
+      TransSys.assumes }
   in
 
   (* Use recursive function with empty accumulator *)
@@ -664,13 +666,34 @@ let call_terms_of_node_call mk_fresh_state_var globals
   in
 
   (* Instantiate assumptions from contracts in this node. *)
-  let node_props =
+  let node_assume_props =
     match contract with
-    | None -> node_props
+    | None -> []
     | Some contract -> (
       subrequirements_of_contract
         call_pos (I.to_scope call_node_name) state_var_map_up contract
-    ) @ node_props
+    )
+  in
+
+  let node_props = node_assume_props @ node_props in
+
+  let node_assumes =
+    if node_assume_props = [] then None
+    else (
+      let assume_terms =
+        List.map (fun { P.prop_term } -> prop_term) node_assume_props
+      in
+      let sofar_term =
+        match contract with
+        | None -> assert false
+        | Some {C.sofar_assump} -> (
+          Var.mk_state_var_instance sofar_assump TransSys.prop_base
+          |> Term.mk_var
+          |> lift_term state_var_map_up
+        )
+      in
+      Some (assume_terms, sofar_term)
+    )
   in
 
   (* Return actual parameters of initial state constraint at bound in
@@ -738,6 +761,7 @@ let call_terms_of_node_call mk_fresh_state_var globals
   state_var_map_down, 
   node_locals, 
   node_props, 
+  node_assumes,
   call_locals,
   init_call_term, 
   init_call_term_trans,
@@ -784,6 +808,7 @@ let rec constraints_of_node_calls
       state_var_map_down,
       node_locals,
       node_props,
+      node_assumes,
       _,
       init_term,
       _,
@@ -806,12 +831,11 @@ let rec constraints_of_node_calls
         call_pos
         state_var_map_up
         state_var_map_down
-
         (* No guarding necessary when instantiating term, because
            this node instance does not have an activation
            condition *)
         (fun _ t -> t)
-
+        node_assumes
         subsystems
     in
 
@@ -839,8 +863,8 @@ let rec constraints_of_node_calls
       with Not_found -> assert false
     in
 
-    let state_var_map_up, state_var_map_down, node_locals, node_props, _,
-        init_term, _, trans_term =
+    let state_var_map_up, state_var_map_down, node_locals, node_props,
+        node_assumes, _, init_term, _, trans_term =
       (* Create node call *)
       call_terms_of_node_call
         mk_fresh_state_var globals node_call node_locals node_props node_def
@@ -873,6 +897,7 @@ let rec constraints_of_node_calls
              [Var.mk_state_var_instance restart i |> Term.mk_var
               |> Term.mk_not;
               t])
+        node_assumes
         subsystems
     in
 
@@ -1003,7 +1028,8 @@ let rec constraints_of_node_calls
       state_var_map_up, 
       state_var_map_down, 
       node_locals, 
-      node_props, 
+      node_props,
+      node_assumes,
       call_locals,
       init_term, 
       init_term_trans, 
@@ -1258,6 +1284,7 @@ let rec constraints_of_node_calls
         state_var_map_up
         state_var_map_down
         guard_clock
+        node_assumes
         subsystems
     in
 

src/nativeInput.ml
@@ -233,7 +233,14 @@ let subsystems_of_sexpr = function
                     (HString.string_of_hstring name))
     in
 
-    let inst = { TransSys.pos = Lib.dummy_pos; map_down; map_up; guard_clock} in
+    let inst = {
+      TransSys.pos = Lib.dummy_pos;
+      map_down;
+      map_up;
+      guard_clock;
+      assumes = None
+    }
+    in
 
     (* assemble subsystem *)
     subsys, inst
@@ -277,7 +284,7 @@ let prop_source_of_sexpr prop_term = function
     let ppos = Lib.pos_of_file_row_col frc_pos in
     let p, scope =
       Lib.extract_scope_name (HString.string_of_hstring scopedprop) in
-    if c == s_assumption then Property.Assumption (ppos, scope)
+    if c == s_assumption then Property.Assumption (ppos, (scope, Lib.dummy_pos))
     else if c == s_guarantee then Property.Guarantee (ppos, scope)
     else if c = s_guaranteeonemodeactive then
       Property.GuaranteeOneModeActive (ppos, scope)

src/property.ml
@@ -69,9 +69,11 @@ and prop_source =
      the name of the property *)
   | Instantiated of Scope.t * t
 
-  (* Contract assumption that a caller has to prove. The list of state vars is
-  the guarantees that proving the requirement yields. *)
-  | Assumption of position * string list
+  (* Contract assumption that a caller has to prove.
+
+     Reference the assumption by its position, the scope of the subsystem,
+     and the position of the node call *)
+  | Assumption of position * (Scope.t * position)
 
   (* Contract guarantees. *)
   | Guarantee of (position * Scope.t)
@@ -123,7 +125,7 @@ let pp_print_prop_source ppf = function
        "instantiated from %s"
               (String.concat "." scope)
 
-  | Assumption (_, scope) ->
+  | Assumption (_, (scope, _)) ->
     Format.fprintf ppf "assumption of %s" (String.concat "." scope)
   | Guarantee (_, scope) ->
     Format.fprintf ppf "guarantee (%a)" Scope.pp_print_scope scope

commit d641634bbd458c6aaaddae7ae1a8a45e74a20656

Fix issue with duplicated properties (introduced in a843ffa)

src/kEvent.ml
@@ -1729,12 +1729,12 @@ let prop_invariant trans_sys prop_name cert =
 
   log_proved mdl L_warn trans_sys None prop_name ;
 
+  (* Update status of property (of all instances with name [prop_name]) *)
+  let status = Property.PropInvariant cert in
+  TransSys.set_prop_status trans_sys prop_name status;
+
   (* Get property by name *)
   let prop = TransSys.property_of_name trans_sys prop_name in
-  let status = Property.PropInvariant cert in
-
-  (* Update status of property in transition system *)
-  Property.set_prop_status prop status;
 
   (* Add property as invariant to transtion system *)
   TransSys.add_invariant trans_sys prop.prop_term cert false |> ignore ;
@@ -2084,10 +2084,8 @@ let update_trans_sys_sub input_sys analysis trans_sys events =
       (* Output proved property *)
       log_proved m L_warn trans_sys None p;
 
-      (* Get property by name *)
-      let prop = TransSys.property_of_name trans_sys p in
-      (* Change property status (in transition system) *)
-      Property.set_prop_status prop s ;
+      (* Change property status (of all instances with name [p]) *)
+      TransSys.set_prop_invariant trans_sys p cert;
 
       let term =
         TransSys.props_list_of_bound trans_sys Numeral.zero
@@ -2106,6 +2104,8 @@ let update_trans_sys_sub input_sys analysis trans_sys events =
       in
 
       let invars =
+        (* Get property by name *)
+        let prop = TransSys.property_of_name trans_sys p in
         match prop.Property.prop_source with
         | Property.Assumption (_, (_, pos)) -> (
           (* If property is a contract assumption that a caller had to prove,