5a6c5ce5b256856e3530cf9eb44536a5ae6ad445
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2021-05-20 09:00:48 -0500

    Bump version numbers to v1.4.0

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,5 @@
 
-# Unreleased
+# Kind 2 v1.4.0
 
 New features:
 
--- doc/usr/source/conf.py
+++ doc/usr/source/conf.py
@@ -19,13 +19,13 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Kind 2'
-copyright = '2015-2020, Board of Trustees of the University of Iowa'
+copyright = '2015-2021, Board of Trustees of the University of Iowa'
 author = 'Kind 2 Team'
 
 # The short X.Y version
-version = 'v1.3'
+version = 'v1.4'
 # The full version, including alpha/beta/rc tags
-release = 'v1.3.1'
+release = 'v1.4.0'
 
 nitpicky = True
 
@@ -250,7 +250,7 @@ latex_elements = {
 
             \sffamily\Large \textbf{\Huge {Kind 2 User Documentation}}
 
-            \sffamily\Large \textbf{Version 1.3.1}
+            \sffamily\Large \textbf{Version 1.4.0}
 
             \newdateformat{monthdayyear}{%
                 \monthname[\THEMONTH] \THEDAY, \THEYEAR}
