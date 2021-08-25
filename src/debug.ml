kind2-mc/kind2

commit 984b4d4d487caec63acea2f0d9172da7c6bdd54d

Remove dependency on SMT solvers for dune tests

src/kind2.ml
@@ -35,6 +35,7 @@ let classify_input_stream: string -> string = fun in_file ->
 
 
 (* Setup everything and returns the input system. Setup includes:
+   - flag parsing,
    - debug setup,
    - log level setup,
    - smt solver setup,
@@ -44,6 +45,9 @@ let classify_input_stream: string -> string = fun in_file ->
    - parsing input file,
    - building input system. *)
 let setup : unit -> any_input = fun () ->
+
+  (* Parse command-line flags. *)
+  Flags.parse_argv () ;
 
   (* Formatter to write debug output to. *)
   (match Flags.debug_log () with

dune-project
@@ -27,8 +27,6 @@ More advanced features include contract-based compositional verification, proof 
   num
   (odoc :with-doc)
   (ounit2 :with-test)
-  (z3
-   (and (< 4.8.9) :with-test))
   yojson
   (zmq
    (>= 5.0.0))

kind2.opam
@@ -22,7 +22,6 @@ depends: [
   "num"
   "odoc" {with-doc}
   "ounit2" {with-test}
-  "z3" {< "4.8.9" & with-test}
   "yojson"
   "zmq" {>= "5.0.0"}
 ]

src/flags.ml
@@ -3699,10 +3699,6 @@ let parse_argv () =
   )
 
 
-(* Parsing command line arguments at load time *)
-let () = parse_argv ()
-
-
 (*
    Local Variables:
    compile-command: "make -C .. -k"

src/flags.mli
@@ -700,11 +700,10 @@ end
 val subdir_for : string list -> string
 
 
-(** {1 Parsing of the command line} 
+(** {1 Parsing of the command line} *)
 
-    Parsing of the command line arguments is performed when loading this
-    module.
-*)
+(** Parse the command line *)
+val parse_argv : unit -> unit
 
 (*
    Local Variables:

commit 14f7d6a921b6338b8b9020d6db6f69db5189969d

Fix operation of Debug module, broken by commit 984b4d4

src/debug.ml
@@ -16,28 +16,51 @@
 
 *)
 
-let dflags = Flags.debug ()
 
-let certif =     List.mem "all" dflags || List.mem "certif" dflags
-let event =      List.mem "all" dflags || List.mem "event" dflags
-let extract =    List.mem "all" dflags || List.mem "extract" dflags
-let fec =        List.mem "all" dflags || List.mem "fec" dflags
-let invgencand = List.mem "all" dflags || List.mem "invgencand" dflags
-let kind2 =      List.mem "all" dflags || List.mem "kind2" dflags
-let ltree =      List.mem "all" dflags || List.mem "ltree" dflags
-let messaging =  List.mem "all" dflags || List.mem "messaging" dflags
-let parse =      List.mem "all" dflags || List.mem "parse" dflags
-let qe =         List.mem "all" dflags || List.mem "qe" dflags
-let qedetailed = List.mem "all" dflags || List.mem "qedetailed" dflags
-let simplify =   List.mem "all" dflags || List.mem "simplify" dflags
-let smt =        List.mem "all" dflags || List.mem "smt" dflags
-let smtexpr =    List.mem "all" dflags || List.mem "smtexpr" dflags
-let transsys =   List.mem "all" dflags || List.mem "transsys" dflags
-let c2i =        List.mem "all" dflags || List.mem "c2i" dflags
-let ic3 =        List.mem "all" dflags || List.mem "ic3" dflags
-let compress =   List.mem "all" dflags || List.mem "compress" dflags
-let native =     List.mem "all" dflags || List.mem "native" dflags
-let contractck = List.mem "all" dflags || List.mem "contractck" dflags
+let certif =     ref false
+let event =      ref false
+let extract =    ref false
+let fec =        ref false
+let invgencand = ref false
+let kind2 =      ref false
+let ltree =      ref false
+let messaging =  ref false
+let parse =      ref false
+let qe =         ref false
+let qedetailed = ref false
+let simplify =   ref false
+let smt =        ref false
+let smtexpr =    ref false
+let transsys =   ref false
+let c2i =        ref false
+let ic3 =        ref false
+let compress =   ref false
+let native =     ref false
+let realiz =     ref false
+
+
+let set_dflags dflags =
+   certif :=     List.mem "all" dflags || List.mem "certif" dflags ;
+   event :=      List.mem "all" dflags || List.mem "event" dflags ;
+   extract :=    List.mem "all" dflags || List.mem "extract" dflags ;
+   fec :=        List.mem "all" dflags || List.mem "fec" dflags ;
+   invgencand := List.mem "all" dflags || List.mem "invgencand" dflags ;
+   kind2 :=      List.mem "all" dflags || List.mem "kind2" dflags ;
+   ltree :=      List.mem "all" dflags || List.mem "ltree" dflags ;
+   messaging :=  List.mem "all" dflags || List.mem "messaging" dflags ;
+   parse :=      List.mem "all" dflags || List.mem "parse" dflags ;
+   qe :=         List.mem "all" dflags || List.mem "qe" dflags ;
+   qedetailed := List.mem "all" dflags || List.mem "qedetailed" dflags ;
+   simplify :=   List.mem "all" dflags || List.mem "simplify" dflags ;
+   smt :=        List.mem "all" dflags || List.mem "smt" dflags ;
+   smtexpr :=    List.mem "all" dflags || List.mem "smtexpr" dflags ;
+   transsys :=   List.mem "all" dflags || List.mem "transsys" dflags ;
+   c2i :=        List.mem "all" dflags || List.mem "c2i" dflags ;
+   ic3 :=        List.mem "all" dflags || List.mem "ic3" dflags ;
+   compress :=   List.mem "all" dflags || List.mem "compress" dflags ;
+   native :=     List.mem "all" dflags || List.mem "native" dflags ;
+   realiz :=     List.mem "all" dflags || List.mem "realiz" dflags
+
 
 let enabled_time = Unix.gettimeofday ()
 
@@ -61,26 +84,26 @@ let printf cond section fmt =
 
 
 (* Instantiated debug functions *)
-let certif fmt = printf certif "certif" fmt
-let event fmt = printf event "event" fmt
-let extract fmt = printf extract "extract" fmt
-let fec fmt = printf fec "fec" fmt
-let invgencand fmt = printf invgencand "invgencand" fmt
-let kind2 fmt = printf kind2 "kind2" fmt
-let ltree fmt = printf ltree "ltree" fmt
-let messaging fmt = printf messaging "messaging" fmt
-let parse fmt = printf parse "parse" fmt
-let qe fmt = printf qe "qe" fmt
-let qedetailed fmt = printf qedetailed "qedetailed" fmt
-let simplify fmt = printf simplify "simplify" fmt
-let smt fmt = printf smt "smt" fmt
-let smtexpr fmt = printf smtexpr "smtexpr" fmt
-let transsys fmt = printf transsys "transsys" fmt
-let c2i fmt = printf c2i "c2i" fmt
-let ic3 fmt = printf ic3 "ic3" fmt
-let compress fmt = printf compress "compress" fmt
-let native fmt = printf native "native" fmt
-let contractck fmt = printf contractck "contractck" fmt
+let certif fmt = printf !certif "certif" fmt
+let event fmt = printf !event "event" fmt
+let extract fmt = printf !extract "extract" fmt
+let fec fmt = printf !fec "fec" fmt
+let invgencand fmt = printf !invgencand "invgencand" fmt
+let kind2 fmt = printf !kind2 "kind2" fmt
+let ltree fmt = printf !ltree "ltree" fmt
+let messaging fmt = printf !messaging "messaging" fmt
+let parse fmt = printf !parse "parse" fmt
+let qe fmt = printf !qe "qe" fmt
+let qedetailed fmt = printf !qedetailed "qedetailed" fmt
+let simplify fmt = printf !simplify "simplify" fmt
+let smt fmt = printf !smt "smt" fmt
+let smtexpr fmt = printf !smtexpr "smtexpr" fmt
+let transsys fmt = printf !transsys "transsys" fmt
+let c2i fmt = printf !c2i "c2i" fmt
+let ic3 fmt = printf !ic3 "ic3" fmt
+let compress fmt = printf !compress "compress" fmt
+let native fmt = printf !native "native" fmt
+let realiz fmt = printf !realiz "realiz" fmt
 
 
         

src/debug.mli
@@ -25,6 +25,8 @@ type 'a t = ('a, Format.formatter, unit) format -> 'a
 (** Set the formatter for debugging *)
 val set_formatter : Format.formatter -> unit
 
+(** Set all debug flags based on input list *)
+val set_dflags : string list -> unit
 
 (** {3 Available debug functions } *)
 
@@ -47,7 +49,7 @@ val c2i : 'a t
 val ic3 : 'a t
 val compress : 'a t
 val native : 'a t
-val contractck : 'a t
+val realiz : 'a t
 
 (* 
    Local Variables:

src/kind2.ml
@@ -49,6 +49,8 @@ let setup : unit -> any_input = fun () ->
   (* Parse command-line flags. *)
   Flags.parse_argv () ;
 
+  Debug.set_dflags (Flags.debug ()) ;
+
   (* Formatter to write debug output to. *)
   (match Flags.debug_log () with
     (* Write to stdout by default. *)

src/realizability.ml
@@ -85,7 +85,7 @@ let compute_and_print_core solver terms =
     unsat_core_lits |> List.map (fun l -> List.assoc l actlit_term_map)
   in
 
-  Debug.contractck "@[<hv>Unsat core:@.@[<hv>%a@]@]@."
+  Debug.realiz "@[<hv>Unsat core:@.@[<hv>%a@]@]@."
     (Lib.pp_print_list Term.pp_print_term "@,") unsat_core_terms
 
 
@@ -214,7 +214,7 @@ let realizability_check
     match ae_val_reponse with
     | QE.Valid _ -> (
 
-      Debug.contractck
+      Debug.realiz
         "@[<hv>Computed fixpoint:@ @[<hv>%a@]@]@." Term.pp_print_term fp ;
 
       let premises' = Term.mk_and free_of_controllable_vars_at_0 in
@@ -228,7 +228,7 @@ let realizability_check
       match ae_val_reponse' with
       | QE.Valid _ -> Realizable fp
       | QE.Invalid _ (* valid_region *) -> (
-        (*Debug.contractck
+        (*Debug.realiz
             "@[<hv>(INITIAL) Valid region:@ @[<hv>%a@]@]@."
             Term.pp_print_term valid_region ;*)
 
@@ -251,7 +251,7 @@ let realizability_check
     )
     | QE.Invalid valid_region -> (
 
-      Debug.contractck
+      Debug.realiz
         "@[<hv>Valid region:@ @[<hv>%a@]@]@." Term.pp_print_term valid_region ;
 
       if uncontrollable_varset_is_non_empty then (
@@ -268,7 +268,7 @@ let realizability_check
 
         match ae_val_reponse' with
         | QE.Valid _ -> (
-          Debug.contractck "@[<hv>Violating region: true@]@." ;
+          Debug.realiz "@[<hv>Violating region: true@]@." ;
 
           (*
           let context = Term.mk_and [premises'; conclusion'] in
@@ -284,7 +284,7 @@ let realizability_check
           Unrealizable (InductiveCase fp1)
         )
         | QE.Invalid violating_region -> (
-          Debug.contractck
+          Debug.realiz
               "@[<hv>Violating region:@ @[<hv>%a@]@]@."
               Term.pp_print_term violating_region ;
 