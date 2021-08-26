kind2-mc/kind2

commit a3b8934af716d56d1673145303cf0d515bbf20f3

Add function that computes unviable trace and core

src/realizability.ml
@@ -17,12 +17,23 @@
 *)
 
 module VS = Var.VarSet
+module ME = ModelElement
 module TSys = TransSys
+module ScMap = Scope.Map
 
+
+type 'a analyze_func =
+  Lib.kind_module list -> 'a InputSystem.t -> Analysis.param -> TransSys.t -> unit
+
+type unrealizable_result =
+  | BaseCase of Term.t
+  (* Valid region for initial states *)
+  | InductiveCase of (Term.t * Term.t)
+  (* One-step valid region without (fst) and with (snd) uncontrollable variables *)
 
 type realizability_result =
   | Realizable of Term.t (* Fixpoint *)
-  | Unrealizable
+  | Unrealizable of unrealizable_result
   | Unknown
 
 
@@ -41,6 +52,7 @@ let rec get_conjucts term =
   | _ -> [term]
 
 
+(*
 let compute_and_print_core solver terms =
   let actlit_term_map =
     terms |> List.map (fun t ->
@@ -124,6 +136,7 @@ let compute_unsat_core_if_debugging sys context requirements ex_var_lst =
   in
   if debugging then
     compute_unsat_core sys context requirements ex_var_lst
+*)
 
 
 let realizability_check
@@ -144,8 +157,6 @@ let realizability_check
     Numeral.zero Numeral.one;
 
   (*Format.printf "%a@." (TSys.pp_print_subsystems true) sys ;*)
-
-  QE.set_ubound Numeral.one ;
 
   let free_of_controllable_vars_at_1, contains_controllable_vars_at_1 =
     let trans =
@@ -181,7 +192,7 @@ let realizability_check
     List.length controllable_vars_at_1 < List.length vars_at_1
   in
 
-  let rec loop fp =
+  let rec loop fp1 fp =
 
     let fp_at_1 = Term.bump_state Numeral.one fp in
 
@@ -217,6 +228,7 @@ let realizability_check
             "@[<hv>(INITIAL) Valid region:@ @[<hv>%a@]@]@."
             Term.pp_print_term valid_region ;*)
 
+        (*
         let neg_region = SMTSolver.simplify_term solver (Term.negate valid_region) in
 
         let context = Term.mk_and [premises'; neg_region] in
@@ -227,8 +239,9 @@ let realizability_check
 
         compute_unsat_core_if_debugging
           sys context requirements controllable_vars_at_0 ;
+        *)
 
-        Unrealizable
+        Unrealizable (InductiveCase fp1)
       )
 
     )
@@ -253,6 +266,7 @@ let realizability_check
         | QE.Valid _ -> (
           Debug.contractck "@[<hv>Violating region: true@]@." ;
 
+          (*
           let context = Term.mk_and [premises'; conclusion'] in
 
           let requirements =
@@ -261,8 +275,9 @@ let realizability_check
 
           compute_unsat_core_if_debugging
             sys context requirements controllable_vars_at_1 ;
+          *)
 
-          Unrealizable
+          Unrealizable (InductiveCase fp1)
         )
         | QE.Invalid violating_region -> (
           Debug.contractck
@@ -273,7 +288,15 @@ let realizability_check
             Term.mk_and [Term.negate violating_region; fp]
             |> SMTSolver.simplify_term solver
           in
-          loop fp'
+
+          let fp1' =
+            if (fst fp1) == Term.t_true then
+              (fp', valid_region)
+            else
+              fp1
+          in
+
+          loop fp1' fp'
         )
 
       )
@@ -283,14 +306,259 @@ let realizability_check
           Term.mk_and [valid_region; fp]
           |> SMTSolver.simplify_term solver
         in
-        loop fp'
+
+        let fp1' =
+          if (fst fp1) == Term.t_true then
+            (fp', fp')
+          else
+            fp1
+        in
+
+        loop fp1' fp'
 
       )
     )
   in
 
-  let res = loop Term.t_true in
+  (* Check initial state constraints *)
+  let premises = Term.mk_and free_of_controllable_vars_at_0 in
+  let conclusion = Term.mk_and contains_controllable_vars_at_0 in
+  match QE.ae_val sys premises controllable_vars_at_0 conclusion with
+  | QE.Valid r ->
+    if r == Term.t_false then (* Premises are inconsistent *)
+      Realizable Term.t_true
+    else (
+      QE.set_ubound Numeral.one ;
+      let res = loop (Term.t_true, Term.t_true)  Term.t_true in
+      QE.on_exit () ;
+      res
+    )
+  | QE.Invalid valid_region ->
+    Unrealizable (BaseCase valid_region)
 
-  QE.on_exit () ;
 
-  res
+let get_contract_cores in_sys sys =
+  let full_loc_core =
+    ME.full_loc_core_for_sys in_sys sys ~only_top_level:true
+  in
+  let guarantee_core, other_core =
+    ME.partition_loc_core_elts_by_guarantees full_loc_core
+  in
+  ME.loc_core_to_new_core guarantee_core,
+  ME.loc_core_to_new_core other_core
+
+
+let get_contract_terms init sys guarantee_core other_core =
+  let scope = TransSys.scope_of_trans_sys sys in
+  let terms_of init g_core o_core =
+    let aux with_act init core =
+      List.fold_left (fun acc s ->
+        let eqs =
+          ME.get_actlits_of_scope core s
+          |> List.map
+            (fun a ->
+              let eq = ME.eq_of_actlit_uf core ~with_act:with_act a in
+              ME.term_of_ts_eq ~init ~closed:(Scope.equal s scope) eq
+            )
+        in
+        ScMap.add s eqs acc
+      ) ScMap.empty (ME.scopes_of_core core)
+    in
+    let g_term = aux true init g_core in
+    let o_term = aux false init o_core in
+    g_term, o_term
+  in
+  terms_of init guarantee_core other_core
+  |> fun (guarantee_sc, other_sc) ->
+    (ScMap.find scope guarantee_sc
+     |> List.map (fun t -> Term.bump_state Numeral.one t),
+     ScMap.find scope other_sc
+     |> List.map (fun t -> Term.bump_state Numeral.one t)
+    )
+
+
+exception Trace_or_core_computation_failed of string
+
+let compute_unviable_trace_and_core
+  analyze in_sys param sys controllable_vars_at_0 controllable_vars_at_1 u_result =
+
+  let vr, cex, is_base, contr_vars =
+    match u_result with
+
+    | BaseCase vr -> vr, [], true, controllable_vars_at_0
+
+    | InductiveCase (vr_wo, vr_w) -> (
+
+      let prop_name = "OneStepVR" in
+
+      let prop =
+        Property.{
+          prop_name ;
+          prop_source = Property.Generated (None, []) ;
+          prop_term = vr_wo ;
+          prop_status = PropUnknown
+        }
+      in
+
+      let sys = TSys.add_properties sys [prop] in
+
+      let modules = [`BMC; `IC3] in
+
+      let old_log_level = Lib.get_log_level () in
+      Lib.set_log_level L_off ;
+
+      analyze modules in_sys param sys ;
+
+      Lib.set_log_level old_log_level;
+
+      match TSys.get_prop_status sys prop_name with
+      | Property.PropFalse cex ->
+
+        vr_w, cex, false, controllable_vars_at_1
+
+      | Property.PropUnknown ->
+        raise (Trace_or_core_computation_failed
+          "Trace computation returned Unknown")
+
+      | _ -> assert false
+    )
+  in
+
+  let cex_assigns =
+    cex |> List.map (fun (sv, values) ->
+      Var.mk_state_var_instance sv Numeral.zero,
+      List.rev values |> List.hd
+    )
+    |> List.map (fun (v, vl) ->
+      match vl with
+      | Model.Term e -> Term.mk_eq [Term.mk_var v; e]
+      | _ -> assert false
+    )
+  in
+
+  let solver =
+    SMTSolver.create_instance
+      ~produce_cores:true
+      ~produce_assignments:true
+      ~minimize_cores:true
+      (TSys.get_logic sys)
+      (Flags.Smt.solver ())
+  in
+
+  TransSys.define_and_declare_of_bounds
+    sys
+    (SMTSolver.define_fun solver)
+    (SMTSolver.declare_fun solver)
+    (SMTSolver.declare_sort solver)
+    Numeral.zero Numeral.one;
+
+  let guarantee_core, other_core = get_contract_cores in_sys sys in
+
+  let guarantee_term, other_term =
+    get_contract_terms is_base sys guarantee_core other_core
+  in
+
+  SMTSolver.push solver ;
+
+  let context =
+    let neg_vr =
+      SMTSolver.simplify_term solver (Term.negate vr)
+    in
+
+    Term.mk_and
+      (neg_vr :: (List.rev_append cex_assigns other_term))
+  in
+
+  SMTSolver.assert_term solver context ;
+
+  assert (SMTSolver.check_sat solver) ;
+
+  let model = SMTSolver.get_model solver in
+
+  SMTSolver.pop solver ;
+
+  let contr_assigns =
+    let ex_var_set = VS.of_list contr_vars in
+    Model.to_list model
+    |> List.filter (fun (v,_) -> VS.mem v ex_var_set |> not)
+    |> List.map (fun (v, vl) ->
+      match vl with
+      | Model.Term e -> Term.mk_eq [Term.mk_var v; e]
+      | _ -> assert false)
+  in
+
+  let unsat_core_lits =
+    let scope = TransSys.scope_of_trans_sys sys in
+    let actlits = ME.get_actlits_of_scope guarantee_core scope in
+    List.iter (SMTSolver.declare_fun solver) actlits ;
+    let requirements =
+      Term.mk_and (List.rev_append contr_assigns guarantee_term)
+    in
+    SMTSolver.assert_term solver requirements ;
+    let act_terms = List.map Actlit.term_of_actlit actlits in
+    SMTSolver.check_sat_assuming solver
+      (fun _ -> assert false)
+      (fun _ -> SMTSolver.get_unsat_core_lits solver)
+      act_terms
+  in
+
+  let unrealizable_core =
+    let actlits =
+      let actlit_of_term t =
+        match Term.destruct t with
+        | Const s -> Symbol.uf_of_symbol s
+        | _ -> assert false
+      in
+      List.map actlit_of_term unsat_core_lits
+    in
+    let core = ME.filter_core actlits guarantee_core in
+    ME.core_to_loc_core in_sys core
+  in
+
+  let cex' =
+    let actlits =
+      let z3_used =
+        match Flags.Smt.solver () with
+        | `Z3_SMTLIB -> true (* Unsat core is minimal *)
+        | _ -> false
+      in
+      if z3_used then
+        (* Drop one activation literal from unsat core, assert the rest to
+           satisfy the maximum number of constraints
+        *)
+        List.tl (List.rev unsat_core_lits)
+      else
+        []
+    in
+
+    SMTSolver.assert_term solver (Term.mk_and actlits);
+
+    assert (SMTSolver.check_sat solver) ;
+
+    let m = SMTSolver.get_model solver in
+
+    match cex with
+    | [] -> (
+      Model.to_list m |> List.map (fun (v, vl) ->
+        Var.state_var_of_state_var_instance v, [vl]
+      )
+    )
+    | _ -> (
+      cex |> List.map (fun (sv, values) ->
+        let v = Var.mk_state_var_instance sv Numeral.one in
+        match Var.VarHashtbl.find_opt m v with
+        | Some vl -> (
+          (* let t = Var.type_of_var v in
+          Format.printf "VAR: %a = %a@."
+            Var.pp_print_var v (Model.pp_print_value ~as_type:t) vl; *)
+          sv, List.rev (vl :: (List.rev values))
+        )
+        | None -> assert false
+      )
+    )
+  in
+
+  SMTSolver.delete_instance solver ;
+
+  cex', unrealizable_core
+

src/kind2Flow.ml
@@ -656,7 +656,7 @@ let run in_sys =
           match result with
           | Realizable _ ->
               KEvent.log_realizable_contract L_warn scope;
-          | Unrealizable -> (
+          | Unrealizable _ -> (
               KEvent.log_unrealizable_contract L_warn scope;
 
               match ContractChecker.check_contract_satisfiability sys with

src/lustre/assumption.ml
@@ -211,7 +211,7 @@ let generate_assumption in_sys sys var_filters prop =
 
     )
   )
-  | Unrealizable -> Failure
+  | Unrealizable _ -> Failure
   | Unknown -> Unknown
 
 

src/lustre/contractChecker.ml
@@ -149,5 +149,5 @@ let check_contract_satisfiability sys =
 
   match res with
   | Realizable _ -> Satisfiable
-  | Unrealizable -> Unsatisfiable
+  | Unrealizable _ -> Unsatisfiable
   | Unknown -> Unknown

src/modelElement.ml
@@ -846,3 +846,19 @@ let filter_loc_core_by_categories main_scope cats loc_core =
         elts
     ) loc_core in
   (ok, not_ok)
+
+let partition_loc_core_elts_by_guarantees loc_core =
+  let f = function
+    | ContractItem (_, _, LustreNode.WeakGuarantee)
+    | ContractItem (_, _, LustreNode.Guarantee) -> true
+    | _ -> false
+  in
+  ScMap.fold
+    (fun scope elts (lc_t, lc_f) ->
+      let elts_t, elts_f =
+        elts |> List.partition (fun (_,_,cat) -> f cat)
+      in
+      ScMap.add scope elts_t lc_t, ScMap.add scope elts_f lc_f
+    )
+    loc_core
+    (ScMap.empty, ScMap.empty)

src/modelElement.mli
@@ -109,6 +109,9 @@ val full_loc_core_for_sys :
 val filter_loc_core_by_categories :
   Scope.t (* Toplevel scope *) -> category list -> loc_core -> loc_core * loc_core
 
+(* Separate contract guarantees from the rest of the model elements *)
+val partition_loc_core_elts_by_guarantees : loc_core -> loc_core * loc_core
+
 (** {1 Pretty Printing} *)
 
 type core_print_data

src/realizability.mli
@@ -21,10 +21,15 @@
     @author Daniel Larraz
 *)
 
+type 'a analyze_func =
+  Lib.kind_module list -> 'a InputSystem.t -> Analysis.param -> TransSys.t -> unit
+
+type unrealizable_result
+
 (** Result of a realizability check *)
 type realizability_result =
   | Realizable of Term.t (* Fixpoint *)
-  | Unrealizable
+  | Unrealizable of unrealizable_result
   | Unknown
 
 (** Checks whether there exists an implementation that satisfies a given specification
@@ -41,4 +46,17 @@ val realizability_check :
   Var.t list ->
   Var.t list ->
   realizability_result
+
+
+exception Trace_or_core_computation_failed of string
+
+val compute_unviable_trace_and_core :
+  'a analyze_func ->
+  'a InputSystem.t ->
+  Analysis.param ->
+  TransSys.t ->
+  Var.t list ->
+  Var.t list ->
+  unrealizable_result ->
+  ((StateVar.t * Model.value list) list * ModelElement.loc_core)
 

commit 0a70e119bbe4317633c8748038c9f25df102c1bb

Fix realizability check when ae_val_use_ctx is false.
Bug introduced by commit a3b8934.

src/realizability.ml
@@ -327,18 +327,24 @@ let realizability_check
   (* Check initial state constraints *)
   let premises = Term.mk_and free_of_controllable_vars_at_0 in
   let conclusion = Term.mk_and contains_controllable_vars_at_0 in
-  match QE.ae_val sys premises controllable_vars_at_0 conclusion with
-  | QE.Valid r ->
-    if r == Term.t_false then (* Premises are inconsistent *)
-      Realizable Term.t_true
-    else (
-      QE.set_ubound Numeral.one ;
-      let res = loop (Term.t_true, Term.t_true)  Term.t_true in
-      QE.on_exit () ;
-      res
-    )
-  | QE.Invalid valid_region ->
-    Unrealizable (BaseCase valid_region)
+
+  QE.set_ubound Numeral.one ; (* Required when Flags.QE.ae_val_use_ctx is false *)
+
+  let res =
+    match QE.ae_val sys premises controllable_vars_at_0 conclusion with
+    | QE.Valid r ->
+      if r == Term.t_false then (* Premises are inconsistent *)
+        Realizable Term.t_true
+      else (
+        loop (Term.t_true, Term.t_true)  Term.t_true
+      )
+    | QE.Invalid valid_region ->
+      Unrealizable (BaseCase valid_region)
+  in
+
+  QE.on_exit () ; (* Required when Flags.QE.ae_val_use_ctx is false *)
+
+  res
 
 
 let get_contract_cores in_sys sys =