5683f86c41d114d802b0d2e873b7875ca3369c95
Rob Lorch <lorchrob@gmail.com>
2022-09-15 14:36:44 -0500

    Clean up error handling and comment

--- src/lustre/lustreAst.ml
+++ src/lustre/lustreAst.ml
@@ -221,7 +221,6 @@ type eq_lhs =
 (* The left-hand side of an equation in a contract *)
 type contract_eq_lhs =
   | GhostVarDec of position * typed_ident list
-  (* Allow untyped identifiers *)
 
 (* An equation or assertion in the node body *)
 type node_equation =
