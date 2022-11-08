kind2-mc/kind2

commit dfc1a9924efe82d9d72a4e70df88261a121d507a

Fix variable ordering issues for Array proofs.

src/certif/proof.ml
@@ -544,8 +544,12 @@ let merge_contexts ctx1 ctx2 =
     else def :: acc
   in
   {
-    lfsc_decls = List.fold_left decl_pred ctx1.lfsc_decls ctx2.lfsc_decls;
-    lfsc_defs = List.fold_left def_pred ctx1.lfsc_defs ctx2.lfsc_defs;
+    lfsc_decls =
+      List.rev
+        (List.fold_left decl_pred (List.rev ctx1.lfsc_decls) ctx2.lfsc_decls);
+    lfsc_defs =
+      List.rev
+        (List.fold_left def_pred (List.rev ctx1.lfsc_defs) ctx2.lfsc_defs);
     fun_defs_args =
       (let h = HH.create 21 in
        HH.iter (HH.add h) ctx1.fun_defs_args;
@@ -553,22 +557,46 @@ let merge_contexts ctx1 ctx2 =
        h);
   }
 
+(* Pretty-print a sort *)
+let rec pp_print_sort ppf t =
+  let t = SMT.interpr_type t in
+  (* Print array types with an abstract sort *)
+  match Type.node_of_type t with
+  | Type.Array (te, ti) ->
+    if Flags.Arrays.smt () then
+      Format.fprintf ppf "(Array %a %a)" pp_print_sort ti pp_print_sort te
+    else
+      Format.fprintf ppf "(cvc.FArray %a %a)" pp_print_sort ti pp_print_sort te
+  | Abstr s -> Format.pp_print_string ppf ("cvc." ^ s)
+  | _ -> Type.pp_print_type ppf t
 
+(* Return a string representation of a sort *)
+let string_of_sort = string_of_t pp_print_sort
+
+(* Generate a simple context with custom indexing of variables to avoid index
+   clashes between proofs generated by cvc5 *)
 let context_from_vars vars =
   let to_decl i v =
+    let rec sexp_of_sort arg_sorts res_sort =
+      match arg_sorts with
+      | [] -> HS.Atom (H.mk_hstring (string_of_sort res_sort))
+      | arg_sort :: arg_sorts ->
+          HS.List
+            [
+              HS.Atom (H.mk_hstring "arrow");
+              HS.Atom (H.mk_hstring (string_of_sort arg_sort));
+              sexp_of_sort arg_sorts res_sort;
+            ]
+    in
     let symb = UfSymbol.name_of_uf_symbol v in
+    let arg_sorts = UfSymbol.arg_type_of_uf_symbol v in
     let res_sort = UfSymbol.res_type_of_uf_symbol v in
     let sort =
       HS.List
         [
           HS.Atom (H.mk_hstring "var");
           HS.Atom (H.mk_hstring (string_of_int i));
-          HS.List
-            [
-              HS.Atom (H.mk_hstring "arrow");
-              HS.Atom (H.mk_hstring (SMT.string_of_sort Type.t_int));
-              HS.Atom (H.mk_hstring (SMT.string_of_sort res_sort));
-            ];
+          sexp_of_sort arg_sorts res_sort;
         ]
     in
     {
@@ -744,14 +772,12 @@ let generate_inv_proof inv =
   Debug.certif "Declaring variables for LFSC contexts";
 
   let ctx_vars = context_from_vars inv.kind2_system.names.vars in
-  fprintf proof_fmt ";; State variables generated by Kind 2\n@.%a\n@."
-  print_context ctx_vars;
 
   Debug.certif "Extracting LFSC contexts from cvc5 proofs";
   
   let ctx_k2 = context_from_file inv.for_system.smt2_lfsc_trace_file in
   fprintf proof_fmt ";; System generated by Kind 2\n@.%a\n@."
-  (print_delta_context ctx_vars) ctx_k2;
+  print_context (merge_contexts ctx_vars ctx_k2);
 
   let ctx_phi = context_from_file inv.phi_lfsc_trace_file in
   fprintf proof_fmt ";; k-Inductive invariant for Kind 2 system\n@.%a\n@."
@@ -866,12 +892,10 @@ let generate_frontend_proof inv =
   let ctx_vars =
     context_from_vars (inv.kind2_system.names.vars @ inv.jkind_system.names.vars)
   in
-  fprintf proof_fmt ";; State variables generated by JKind\n@.%a\n@."
-  print_context ctx_vars;
 
-  let ctx_jk = merge_contexts (context_from_file inv.jkind_system.smt2_lfsc_trace_file) ctx_vars in
+  let ctx_jk = context_from_file inv.jkind_system.smt2_lfsc_trace_file in
   fprintf proof_fmt ";; System generated by JKind\n@.%a\n@."
-  (print_delta_context ctx_vars) ctx_jk;
+  print_context (merge_contexts ctx_vars ctx_jk);
 
   let ctx_obs = context_from_file inv.obs_system.smt2_lfsc_trace_file in
   fprintf proof_fmt ";; System generated for Observer\n@.%a\n@."

src/certif/certifChecker.ml
@@ -1772,11 +1772,8 @@ let generate_split_certificates sys dirname =
   Stat.set (Certificate.size (k, phi)) Stat.certif_size;
 
   let svars =
-    List.filter_map
-      (fun v ->
-        if StateVar.is_const v then None
-        else Some (StateVar.uf_symbol_of_state_var v))
-      (TS.state_vars sys)
+    StateVar.get_select_ufs ()
+    @ List.map StateVar.uf_symbol_of_state_var (TS.state_vars sys)
   in
 
   let names_kind2 = names_kind2 svars in
@@ -2798,16 +2795,17 @@ let generate_frontend_certificates sys dirname =
   (* Time statistics *)
   Stat.record_time Stat.certif_gen_time;
 
-  let pred v =
-    if StateVar.is_const v then None
-    else Some (StateVar.uf_symbol_of_state_var v)
+  (* We do not consider select functions here because the Syntax and semantics
+     of arrays in Kind 2 and Jkind are different *)
+  let kind2_sys = List.nth (TS.get_subsystems sys) 1 in
+  let kind2_svars =
+    List.map StateVar.uf_symbol_of_state_var (TS.state_vars kind2_sys)
   in
 
-  let kind2_sys = List.nth (TS.get_subsystems sys) 1 in
-  let kind2_svars = List.filter_map pred (TS.state_vars kind2_sys) in
-
   let jkind_sys = List.nth (TS.get_subsystems sys) 0 in
-  let jkind_svars = List.filter_map pred (TS.state_vars jkind_sys) in
+  let jkind_svars =
+    List.map StateVar.uf_symbol_of_state_var (TS.state_vars jkind_sys)
+  in
 
   let obs_sys = obs_cert_sys dirname in
 

commit 29b56be83a1f8dc050532b0abd76ac7186133c91

Fix printing of bitvector sort in LFSC proofs
Issue introduced in commit dfc1a99

src/certif/proof.ml
@@ -568,6 +568,7 @@ let rec pp_print_sort ppf t =
     else
       Format.fprintf ppf "(cvc.FArray %a %a)" pp_print_sort ti pp_print_sort te
   | Abstr s -> Format.pp_print_string ppf ("cvc." ^ s)
+  | UBV i | BV i -> Format.fprintf ppf "(BitVec %d)" i
   | _ -> Type.pp_print_type ppf t
 
 (* Return a string representation of a sort *)