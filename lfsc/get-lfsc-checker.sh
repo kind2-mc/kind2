9646b9ad13f1c02cf09ad775d7e4504f6016f028
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2022-11-18 09:27:50 -0600

    Disable cvc5 version check temporarily

--- lfsc/get-lfsc-checker.sh
+++ lfsc/get-lfsc-checker.sh
@@ -35,7 +35,7 @@ make install
 ##### signatures
 
 # The LFSC signatures live in the main cvc5 repository
-version="5b71570ad5222815ede7f5fa1297e27c5840a381"
+version="25e52a430a29c9471ecf772b0bd913417a3be2af"
 SIG_DIR_URL="https://raw.githubusercontent.com/cvc5/cvc5/$version/proofs/lfsc/signatures"
 
 # install signatures and scripts
