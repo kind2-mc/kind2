f01d69be87e64a1615d579a8dc10a69a5810a33e
Rob Lorch <lorchrob@gmail.com>
2024-02-14 16:53:57 -0600

    Finish generation of imported nodes

--- src/lustre/lustreGenRefTypeImpNodes.ml
+++ src/lustre/lustreGenRefTypeImpNodes.ml
@@ -36,10 +36,12 @@ let unwrap_res =
 
 let ref_type_to_contract: Ctx.tc_context -> A.lustre_type -> HString.t option -> A.declaration option
 = fun ctx ty node_id -> match ty with 
-  | RefinementType (pos, (_, id, ty), expr) -> 
+  | RefinementType (pos, (_, id, ty), expr) as ref_type -> 
     let span = { A.start_pos = Lib.dummy_pos; end_pos = Lib.dummy_pos } in
+    let ty_str = Lib.string_of_t A.pp_print_lustre_type ref_type |> HString.mk_hstring in
     let gen_node_id = HString.concat2 (HString.mk_hstring (string_of_int !i)) 
-                                      (HString.mk_hstring "_node") in
+                                      ty_str in
+    i := !i + 1;
     let is_extern = true in
     let params = [] in 
     let vars = AH.vars_without_node_call_ids expr in
@@ -59,7 +61,7 @@ let ref_type_to_contract: Ctx.tc_context -> A.lustre_type -> HString.t option ->
     ) inputs in
     let outputs = [(pos, id, ty, A.ClockTrue)] in
     let locals = [] in 
-    let node_items = [] in 
+    let node_items = [A.AnnotMain(pos, true)] in 
     (* Add assumption for each variable with a refinement type in 'expr' *)
     let assumptions = List.filter_map (fun (_, id, _, _, _) -> 
       let ty = Ctx.lookup_ty ctx id |> unwrap in 
@@ -75,17 +77,20 @@ let ref_type_to_contract: Ctx.tc_context -> A.lustre_type -> HString.t option ->
     Some (NodeDecl (span, (gen_node_id, is_extern, params, inputs, outputs, locals, node_items, contract)))
   | _ -> None
 
-let handle_inputs: Ctx.tc_context -> HString.t -> A.const_clocked_typed_decl -> A.declaration option list 
-= fun ctx node_id decl ->
-  assert false
+let handle_inputs: Ctx.tc_context -> HString.t -> A.const_clocked_typed_decl -> A.declaration option 
+= fun ctx node_id (_, _, ty, _, _) ->
+  ref_type_to_contract ctx ty (Some node_id)
 
-let handle_outputs: Ctx.tc_context -> HString.t -> A.clocked_typed_decl -> A.declaration option list 
-= fun ctx node_id decl ->
-  assert false
+let handle_outputs: Ctx.tc_context -> HString.t -> A.clocked_typed_decl -> A.declaration option 
+= fun ctx node_id (_, _, ty, _) ->
+  ref_type_to_contract ctx ty (Some node_id)
 
-let handle_locals: Ctx.tc_context -> HString.t -> A.node_local_decl -> A.declaration option list 
-= fun ctx node_id decl ->
-  assert false
+let handle_locals: Ctx.tc_context -> HString.t -> A.node_local_decl -> A.declaration option 
+= fun ctx node_id -> function 
+  | A.NodeVarDecl (_, (_, _, ty, _)) 
+  | A.NodeConstDecl (_, TypedConst (_, _, _, ty))
+  | A.NodeConstDecl (_, FreeConst (_, _, ty)) -> ref_type_to_contract ctx ty (Some node_id)
+  | A.NodeConstDecl (_, UntypedConst _) -> None
 
 let gen_imp_nodes: Ctx.tc_context -> A.declaration list -> A.declaration list 
 = fun ctx decls -> 
@@ -97,10 +102,10 @@ let gen_imp_nodes: Ctx.tc_context -> A.declaration list -> A.declaration list
       | A.ConstDecl (_, TypedConst (_, _, _, ty)) -> [(ref_type_to_contract ctx ty None)]
       | A.TypeDecl (_, FreeType _)
       | A.ConstDecl (_, UntypedConst _) -> []
-      | A.NodeDecl (_, (id, _, _, inputs, outputs, locals, _, _))
-      | A.FuncDecl (_, (id, _, _, inputs, outputs, locals, _, _)) -> 
+      | A.NodeDecl (_, ((id, _, _, inputs, outputs, locals, _, _) as decl))
+      | A.FuncDecl (_, ((id, _, _, inputs, outputs, locals, _, _) as decl)) -> 
+        let ctx = Chk.get_node_ctx ctx decl |> unwrap_res in
         List.map (handle_inputs ctx id) inputs @ List.map (handle_outputs ctx id) outputs @ List.map (handle_locals ctx id) locals
-        |> List.flatten
       | A.ContractNodeDecl _ 
       | A.NodeParamInst _ -> [] (*TODO*)
     ) decls |> List.flatten |> List.filter_map Fun.id
