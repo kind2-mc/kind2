kind2-mc/kind2

commit be290ed87e9bb9d284ed3a7a76b949bc0035f8d6

Add node type parameters to all contexts

src/lustre/typeCheckerContext.ml
@@ -782,13 +782,10 @@ and ty_vars_of_type ctx node_name ty =
   let call = ty_vars_of_type ctx node_name in 
   match ty with
   | UserType (_, ty_args, id) -> (
-    let ty_vars =
-      List.fold_left SI.union SI.empty (List.map call ty_args)
-    in
-    match lookup_ty_syn ctx node_name ty_args with 
-    | Some _ -> ty_vars
-    | None -> SI.add id ty_vars
-    )
+    match lookup_ty_syn ctx id ty_args with
+    | Some ty -> ty_vars_of_type ctx node_name ty
+    | None -> assert false
+  )
   | RefinementType (_, (_, _, ty), e) 
   | ArrayType (_, (ty, e)) -> 
     SI.union (call ty) (ty_vars_of_expr ctx node_name e)
@@ -798,5 +795,12 @@ and ty_vars_of_type ctx node_name ty =
     let vars = List.map (fun (_, _, ty) -> call ty) tis in 
     List.fold_left SI.union SI.empty vars
   | TArr (_, ty1, ty2) -> SI.union (call ty1) (call ty2)
+  | AbstractType (_, id) -> (
+    match lookup_node_ty_vars ctx node_name with
+    | Some tvars ->
+      if List.mem id tvars then SI.singleton id
+      else SI.empty
+    | None -> SI.empty
+  )
   | History _ | Int _ | Int8 _ | Int16 _ | Int32 _ | Int64 _ | UInt8 _ | UInt16 _ | UInt32 _ | UInt64 _ 
-  | Bool _ | IntRange _ | Real _ | AbstractType _ | EnumType _ -> SI.empty
+  | Bool _ | IntRange _ | Real _  | EnumType _ -> SI.empty

src/lustre/lustreAbstractInterpretation.ml
@@ -229,14 +229,14 @@ and interpret_decl ty_ctx gids = function
   | ContractNodeDecl (_, decl) -> interpret_contract_node ty_ctx decl
   | NodeParamInst _ -> empty_context
 
-and interpret_contract_node ty_ctx (id, _, ins, outs, contract) =
+and interpret_contract_node ty_ctx (id, ps, ins, outs, contract) =
   (* Setup the typing context *)
-  let ty_ctx = TC.add_io_node_ctx ty_ctx ins outs in
+  let ty_ctx = TC.add_io_node_ctx ty_ctx id ps ins outs in
   interpret_contract id empty_context ty_ctx contract
 
-and interpret_node ty_ctx gids (id, _, _, ins, outs, locals, items, contract) =
+and interpret_node ty_ctx gids (id, _, ps, ins, outs, locals, items, contract) =
   (* Setup the typing context *)
-  let ty_ctx = TC.add_io_node_ctx ty_ctx ins outs in
+  let ty_ctx = TC.add_io_node_ctx ty_ctx id ps ins outs in
   let ctx = IMap.empty in
   let contract_ctx = match contract with
     | Some contract -> interpret_contract id ctx ty_ctx contract 

src/lustre/lustreArrayDependencies.ml
@@ -268,9 +268,9 @@ let rec check_inductive_array_dependencies ctx ns = function
   | [] -> Ok ()
 
 and check_node_decl ctx ns decl =
-  let (_, _, _, inputs, outputs, locals, items, _) = decl in
+  let (id, _, params, inputs, outputs, locals, items, _) = decl in
   (* Setup the typing context *)
-  let ctx = Chk.add_full_node_ctx ctx inputs outputs locals in
+  let ctx = Chk.add_full_node_ctx ctx id params inputs outputs locals in
   let* (graph, pos_map, count, idx_len) = process_items ctx ns items in
   (* Format.eprintf "Initial graph: %a@." G.pp_print_graph graph; *)
   let graph = add_init_edges idx_len graph in

src/lustre/lustreAstNormalizer.ml
@@ -959,8 +959,8 @@ and normalize_declaration info map = function
   | FuncDecl (span, decl) ->
     let normal_decl, map, warnings = normalize_node info map decl in
     Some (A.FuncDecl (span, normal_decl)), map, warnings
-  | ContractNodeDecl (_, (id, _, ips, ops, _)) ->
-    let ctx = Chk.add_io_node_ctx info.context ips ops in
+  | ContractNodeDecl (_, (id, ps, ips, ops, _)) ->
+    let ctx = Chk.add_io_node_ctx info.context id ps ips ops in
     let info = { info with context = ctx } in
     let ngids, warnings = normalize_gid_equations info map id in
     let map = StringMap.add id ngids map in
@@ -1059,7 +1059,7 @@ and normalize_ghost_declaration info node_id map = function
 and normalize_node info map
     (node_id, is_extern, params, inputs, outputs, locals, items, contract) =
   (* Setup the typing context *)
-  let ctx = Chk.add_io_node_ctx info.context inputs outputs in
+  let ctx = Chk.add_io_node_ctx info.context node_id params inputs outputs in
   let ctx = Ctx.add_ty ctx ctr_id (A.Int dpos) in
   let info = { info with context = ctx } in
   (* Normalize types *)

src/lustre/lustreDesugarAnyOps.ml
@@ -93,7 +93,6 @@ fun ctx node_name fun_ids expr ->
     | Some expr2 -> ty_params @ Ctx.SI.elements (Ctx.ty_vars_of_expr ctx node_name expr2)
     | None -> ty_params 
     in
-    let ty_params = List.filter (fun p -> not (Ctx.member_ty_syn ctx p)) ty_params in 
     let ty_vars = List.map (fun id -> A.UserType (pos, [], id)) ty_params in
     let generated_node = 
       if has_pre_arrow_or_node_call then
@@ -271,19 +270,19 @@ fun ctx decls ->
   List.fold_left (fun decls decl ->
     match decl with
     | A.NodeDecl (span, (id, ext, params, inputs, outputs, locals, items, contract)) -> 
-      let ctx = Chk.add_full_node_ctx ctx inputs outputs locals in
+      let ctx = Chk.add_full_node_ctx ctx id params inputs outputs locals in
       let items, gen_nodes = List.map (desugar_node_item ctx id fun_ids) items |> List.split in 
       let contract, gen_nodes2 = desugar_contract ctx id fun_ids contract in
       let gen_nodes = List.flatten gen_nodes in
       decls @ gen_nodes @ gen_nodes2 @ [A.NodeDecl (span, (id, ext, params, inputs, outputs, locals, items, contract))]
     | A.FuncDecl (span, (id, ext, params, inputs, outputs, locals, items, contract)) -> 
-      let ctx = Chk.add_full_node_ctx ctx inputs outputs locals in
+      let ctx = Chk.add_full_node_ctx ctx id params inputs outputs locals in
       let items, gen_nodes = List.map (desugar_node_item ctx id fun_ids) items |> List.split in 
       let contract, gen_nodes2 = desugar_contract ctx id fun_ids contract in
       let gen_nodes = List.flatten gen_nodes in
       decls @ gen_nodes @ gen_nodes2 @ [A.FuncDecl (span, (id, ext, params, inputs, outputs, locals, items, contract))]
     | A.ContractNodeDecl (span, (id, params, inputs, outputs, contract)) ->
-      let ctx = Chk.add_io_node_ctx ctx inputs outputs in
+      let ctx = Chk.add_io_node_ctx ctx id params inputs outputs in
       let contract, gen_nodes = desugar_contract ctx id fun_ids (Some contract) in
       let contract = match contract with
       | Some contract -> contract

src/lustre/lustreDesugarIfBlocks.ml
@@ -342,13 +342,13 @@ let rec desugar_node_item node_id ctx ni = match ni with
 (** Desugars an individual node declaration (removing IfBlocks). *)
 let desugar_node_decl ctx decl = match decl with
   | A.FuncDecl (s, (node_id, b, nps, cctds, ctds, nlds, nis, co)) ->
-    let ctx = Chk.add_full_node_ctx ctx cctds ctds nlds in
+    let ctx = Chk.add_full_node_ctx ctx node_id nps cctds ctds nlds in
     let* nis = R.seq (List.map (desugar_node_item node_id ctx) nis) in
     let new_decls, nis, gids = split_and_flatten3 nis in
     let gids = List.fold_left GI.union (GI.empty ()) gids in
     R.ok (A.FuncDecl (s, (node_id, b, nps, cctds, ctds, new_decls @ nlds, nis, co)), GI.StringMap.singleton node_id gids) 
   | A.NodeDecl (s, (node_id, b, nps, cctds, ctds, nlds, nis, co)) ->
-    let ctx = Chk.add_full_node_ctx ctx cctds ctds nlds in
+    let ctx = Chk.add_full_node_ctx ctx node_id nps cctds ctds nlds in
     let* nis = R.seq (List.map (desugar_node_item node_id ctx) nis) in
     let new_decls, nis, gids = split_and_flatten3 nis in
     let gids = List.fold_left GI.union (GI.empty ()) gids in

src/lustre/lustreFlattenRefinementTypes.ml
@@ -141,7 +141,13 @@ let flatten_ref_types ctx sorted_node_contract_decls =
   List.map (fun decl -> match decl with
     | A.TypeDecl (pos, AliasType (pos2, id, ps, ty)) -> 
       A.TypeDecl (pos, AliasType (pos2, id, ps, flatten_ref_type ctx ty))
-    | NodeDecl (pos, (id, imported, params, ips, ops, locals, items, contract)) -> 
+    | NodeDecl (pos, (id, imported, params, ips, ops, locals, items, contract)) ->
+      let ctx =
+        List.fold_left (fun acc p ->
+          TypeCheckerContext.add_ty_syn acc p (A.AbstractType (Lib.dummy_pos, p))
+        )
+        ctx params
+      in
       let ips = List.map (fun (pos, id, ty, cl, b) -> 
         (pos, id, flatten_ref_type ctx ty, cl, b)
       ) ips in
@@ -151,7 +157,13 @@ let flatten_ref_types ctx sorted_node_contract_decls =
       let locals = List.map (flatten_ref_types_local_decl ctx) locals in
       let items = List.map (flatten_ref_types_item ctx) items in
       NodeDecl (pos, (id, imported, params, ips, ops, locals, items, contract))
-    | FuncDecl (pos, (id, imported, params, ips, ops, locals, items, contract)) -> 
+    | FuncDecl (pos, (id, imported, params, ips, ops, locals, items, contract)) ->
+      let ctx =
+        List.fold_left (fun acc p ->
+          TypeCheckerContext.add_ty_syn acc p (A.AbstractType (Lib.dummy_pos, p))
+        )
+        ctx params
+      in
       let ips = List.map (fun (pos, id, ty, cl, b) -> 
         (pos, id, flatten_ref_type ctx ty, cl, b)
       ) ips in

src/lustre/lustreInstantiatePolyNodes.ml
@@ -485,7 +485,8 @@ and gen_poly_decls_ci
 and gen_poly_decls_decls
 = fun ctx node_decls_map decls -> 
   let ctx, decls, node_decls_map = List.fold_left (fun (ctx, acc_decls, acc_node_decls_map) decl -> match decl with
-  | A.FuncDecl (p, (nname, ext, ps, ips, ops, locs, nis, c)) -> 
+  | A.FuncDecl (p, (nname, ext, ps, ips, ops, locs, nis, c)) ->
+    let ctx = Chk.add_ty_params_node_ctx ctx nname ps in
     let ctx, ips, decls, node_decls_map = List.fold_left (fun (ctx, acc_ips, acc_decls, acc_node_decls_map) ip -> 
       let ctx, ip, decls, node_decls_map = gen_poly_decls_ip ctx (Some nname) acc_node_decls_map ip in 
       ctx, acc_ips @ [ip], decls @ acc_decls, node_decls_map
@@ -514,7 +515,8 @@ and gen_poly_decls_decls
       let decl = A.FuncDecl (p, (nname, ext, ps, ips, ops, locs, nis, Some (p3, c))) in
       ctx, decl :: decls, node_decls_map
     )
-  | NodeDecl (p, (nname, ext, ps, ips, ops, locs, nis, c)) -> 
+  | NodeDecl (p, (nname, ext, ps, ips, ops, locs, nis, c)) ->
+    let ctx = Chk.add_ty_params_node_ctx ctx nname ps in
     let ctx, ips, decls, node_decls_map = List.fold_left (fun (ctx, acc_ips, acc_decls, acc_node_decls_map) ip -> 
       let ctx, ip, decls, node_decls_map = gen_poly_decls_ip ctx (Some nname) acc_node_decls_map ip in 
       ctx, acc_ips @ [ip], decls @ acc_decls, node_decls_map
@@ -543,7 +545,8 @@ and gen_poly_decls_decls
         let decl = A.NodeDecl (p, (nname, ext, ps, ips, ops, locs, nis, Some (p3, c))) in
         ctx, decl :: decls, node_decls_map
     )
-  | ContractNodeDecl (p, (cname, ps, ips, ops, (p3, c))) -> 
+  | ContractNodeDecl (p, (cname, ps, ips, ops, (p3, c))) ->
+    let ctx = Chk.add_ty_params_node_ctx ctx cname ps in
     let ctx, ips, decls, node_decls_map = List.fold_left (fun (ctx, acc_ips, acc_decls, acc_node_decls_map) ip -> 
       let ctx, ip, decls, node_decls_map = gen_poly_decls_ip ctx (Some cname) acc_node_decls_map ip in 
       ctx, acc_ips @ [ip], decls @ acc_decls, node_decls_map

commit e800fb435de3ecf2e3d3e656d5d473d72ab622b2

Fix check of undeclared types in any operators
Bug introduced in be290ed. Notice that desugaring of
any operators occurs before type checking.
Previous code, `SI.add id ty_vars`, does not work because it leads to
incorrect positions being reported in error messages.

src/lustre/typeCheckerContext.ml
@@ -807,7 +807,7 @@ and ty_vars_of_type ctx node_name ty =
   | UserType (_, ty_args, id) -> (
     match lookup_ty_syn ctx id ty_args with
     | Some ty -> ty_vars_of_type ctx node_name ty
-    | None -> assert false
+    | None -> SI.empty
   )
   | RefinementType (_, (_, _, ty), e) 
   | ArrayType (_, (ty, e)) -> 

tests/ounit/lustre/lustreTypeChecker/undeclared_type_06.lus
@@ -1,0 +1,4 @@
+node N() returns (y: int);
+let
+  y = any Foo;
+tel

tests/ounit/lustre/testLustreFrontend.ml
@@ -599,6 +599,10 @@ let _ = run_test_tt_main ("frontend LustreTypeChecker error tests" >::: [
     match load_file "./lustreTypeChecker/undeclared_type_05.lus" with
     | Error (`LustreTypeCheckerError (_, UndeclaredType _)) -> true
     | _ -> false);
+  mk_test "test undeclared 6" (fun () ->
+    match load_file "./lustreTypeChecker/undeclared_type_06.lus" with
+    | Error (`LustreTypeCheckerError (_, UndeclaredType _)) -> true
+    | _ -> false);
   mk_test "test arity incorrect node call" (fun () ->
     match load_file "./lustreTypeChecker/arity_incorrect_node_call.lus" with
     | Error (`LustreTypeCheckerError (_, IlltypedCall _)) -> true