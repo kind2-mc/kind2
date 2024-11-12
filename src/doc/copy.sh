548347e8af0b99d057428e16d11525b1e620d6c0
Ethan Rooke <ethan@roo.ke>
2024-11-12 11:07:38 -0600

    docs: quote args in src/docs/copy.sh

--- src/doc/copy.sh
+++ src/doc/copy.sh
@@ -3,9 +3,9 @@ html_path=../../_doc/_html
 # get name of internal library
 lib_name=$(ls $html_path | grep kind2dev)
 # copy ./include dir into library documentation dir
-cp -r ./include $html_path/$lib_name
+cp -r ./include "$html_path/$lib_name"
 # compile index.mld into page-index.odoc file
-odoc compile --pkg=$lib_name index.mld
+odoc compile --pkg="$lib_name" index.mld
 # convert page-index.odoc to index.html and resolve links to other webpages
 odoc html ./page-index.odoc -I ../.kind2dev.objs/byte --output $html_path
 # replace all instances of kind2 with lib_name
