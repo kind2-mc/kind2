c5deacf5ae59bab36febf5517529b05be295f0f5
Joshua Bergthold <jbergthold@uiowa.edu>
2026-01-23 12:10:11 -0600

    Filter Kind 2 function defs from frontend proofs

--- src/certif/proof.ml
+++ src/certif/proof.ml
@@ -1157,6 +1157,40 @@ let factor_jkind_defs (proof : cpc_step list) =
   in
   aux ([]) proof
 
+  let is_kind_2_def_name (s : HString.t) =
+    let open ReservedIds in
+  
+    let n = HString.string_of_hstring s in
+    let result =
+    (String.starts_with ~prefix: init_uf_string n)
+    ||
+     (String.starts_with ~prefix: trans_uf_string n)
+    in
+
+    Format.printf "%B :: %s" result n;
+    result
+  let is_kind_2_def = function 
+  
+  | HS.List [HS.Atom kw; HS.Atom name; _] as step
+      when HString.equal kw (HString.mk_hstring "define") ->
+        Format.printf "CHecking if its a kind 2 def (true-ish): %a\n"  HS.pp_print_sexpr step;
+        is_kind_2_def_name name
+    | step -> 
+      Format.printf "CHecking if its a kind 2 def (false) %a\n" HS.pp_print_sexpr step;
+        false
+
+  let remove_kind_2_defs (proof : cpc_step list) =
+  let rec aux (acc) = function
+    | sexpr :: tl when sexpr |> is_kind_2_def->
+       aux acc tl
+    | sexpr :: tl ->
+        aux (sexpr :: acc) tl
+    | [] ->
+        List.rev acc
+  in
+  aux ([]) proof
+
+
 let get_last_step_name steps =
   let rec find_last = function
     | [] ->
@@ -1313,15 +1347,22 @@ let construct_kind_2_proof dirname base induction implication =
 
   
 
-    let (defs, base_steps) = get_proof_defs base_jkind in
+    let (defs, base_steps) = get_proof_defs base_jkind  in
     let (_, induction_steps) = get_proof_defs induction_jkind in
     let (_, implication_steps) = get_proof_defs implication_jkind in
     let (_, jkind_defs) = factor_jkind_defs defs in
+
+    let base_steps = remove_kind_2_defs base_steps in
+    let induction_steps = remove_kind_2_defs induction_steps in
+    let implication_steps = remove_kind_2_defs implication_steps in
+    Format.printf "Calling remove kind 2 defs\n";
+    let trimmed_jkind_defs = remove_kind_2_defs jkind_defs in
     
  let oc = open_out (dirname ^ "/frontend_proof.cpc") in
   let fmt = Format.formatter_of_out_channel oc in
     Format.printf "%s\n" dirname ;
-  pp_print_frontend_proof fmt jkind_defs base_steps induction_steps implication_steps
+    Format.printf "JKIND defs (trimmed): %a\n" pp_cpc_proof trimmed_jkind_defs;
+  pp_print_frontend_proof fmt trimmed_jkind_defs base_steps induction_steps implication_steps
 
 let parse_cpc_file (filename : string) : cpc_step list =
   let ic = open_in filename in
