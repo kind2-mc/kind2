9771c0655e782e1e5e9bcd00defafcdaf437e4cd
Robert Lorch <47061706+lorchrob@users.noreply.github.com>
2026-02-23 18:52:02 -0500

    Generate refinement type constraints for map and set type annotations (#1291)
    See test case. There is one internal comment intentionally left in for
    now (starting with `(*!! ...*)`). I think it is alright, but please read
    and confirm if my understanding is correct.
    
    There was an ordering issue between normalization of types and erasure
    of refinement type bound variables. Consider a type annotation `{ 1
    }@<subtype { x: int | x = pre x }>`. If we normalize the refinement type
    and then substitute, we get the constraint `1 = pre 1`. However, this
    violates the (post-normalization) assumption that an argument to `pre`
    is an identifier. So, we have to generate the refinement type
    constraints, and then normalize after. To do this, we have to call
    `normalize_expr` within `mk_fresh_refinement_type_constraint`; however,
    doing this required moving `mk_fresh_refinement_type_constraint` within
    the file so that `normalize_expr` would be in scope. I performed
    analogous handling for subranges.
    
    EDIT -- internal comment addressed

--- src/lustre/lustreNodeGen.ml
+++ src/lustre/lustreNodeGen.ml
@@ -1410,7 +1410,7 @@ and compile_ast_expr
   | A.IndexAccess (_, expr, k, Map) ->
     let expr = compile_map_index bounds expr k in
     X.find_prefix [(X.TupleIndex 1)] expr
-  | A.IndexAccess _ -> assert false
+  | A.IndexAccess (_, _, _, Unknown) -> assert false
   (* ****************************************************************** *)
   (* Not Implemented                                                    *)
   (* ****************************************************************** *)
