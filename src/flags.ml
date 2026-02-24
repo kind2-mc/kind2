c323106db5618914af388d49935a21dbc8023cc0
Joshua Bergthold <jbergthold@uiowa.edu>
2026-02-24 13:36:07 -0600

    Update cvc5 version requirements

--- src/flags.ml
+++ src/flags.ml
@@ -4069,11 +4069,11 @@ let solver_dependent_actions solver =
           Certif.proof () && not (
             (* major=1 && minor=0 && patch>=3 && patch<=9 ||
             major=1 && minor=1 && patch=0 *)
-             major=1 && minor=3 && patch=1
+             major=1 && minor=3 && patch>=2
           )
         then (
           Log.log L_error
-            "LFSC proof production requires cvc5 >= 1.0.3 and <= 1.1.0. Found \
+            "Eunoia proof production requires cvc5 >= 1.3.2. Found \
              version: %d.%d.%d"
             major minor patch;
           raise UnsupportedSolver) ;
