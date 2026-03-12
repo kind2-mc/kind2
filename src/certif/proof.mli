19d649a41cdba49731da98045bdf433006c09bae
Joshua Bergthold <jbergthold@uiowa.edu>
2026-03-12 15:27:16 -0500

    Add comments to proof mli

--- src/certif/proof.mli
+++ src/certif/proof.mli
@@ -15,13 +15,20 @@
    permissions and limitations under the License. 
 
 *)
+
 (** Set the logic used by the proof generations mechanism *)
 val set_proof_logic : TermLib.logic -> unit
 
+(** Filename of the final proof of safety *)
 val safety_proofname_cpc : string
 
+(** Creates the kind 2 CPC proofs of base, induction , and implication. 
+Also outputs the combined "Safety CPC" proof of Kind 2 *)
 val construct_kind_2_proof:   string -> string -> string -> string -> int -> unit
 
+(** Creates the frontend (JKind) CPC proofs of base, induction , and implication. 
+Also outputs the combined "Safety CPC" proof of the frontend  *)
 val construct_frontend_proof: string -> string -> string -> string -> int -> unit
 
+(** Combines the kind 2 and frontend safety CPC proofs into one proof of Safety in Safety CPC*)
 val construct_safety_proof: string -> unit
