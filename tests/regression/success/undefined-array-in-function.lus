kind2-mc/kind2

commit dc1ab06029638705a198ad23aa5d52d06c5ed4f1

Store UF symbols for functions in LustreNode (#1286)

src/lustre/lustreTransSys.ml
@@ -2084,7 +2084,7 @@ let rec trans_sys_of_node' options globals top_name analysis_param
             N.props;
             N.history_svars;
             N.contract;
-            N.is_function } as node =
+            N.comp_type } as node =
 
         try 
 
@@ -2213,52 +2213,34 @@ let rec trans_sys_of_node' options globals top_name analysis_param
             List.filter is_undefined (D.values outputs)
           in
 
-          (* If node is a function, create a UF `f` for each undefined output. Also,
+          (* If node is a function, for each undefined output,
           create the term `(= (f <inputs>) output)` to add it to `init` and
           `trans`. *)
           let function_ufs, function_constraints_at_0 =
-
-            if not is_function || not options.add_functional_constraints then
-              [], []
-            else (
-              let inputs = D.values inputs in
-              let type_of = StateVar.type_of_state_var in
-              let term_0_of svar =
-                Var.mk_state_var_instance svar Numeral.zero
-                |> Term.mk_var
-              in
-              let input_types, input_terms_at_0 =
-                inputs
-                |> List.rev
-                |> List.fold_left (
-                  fun (types, terms) input ->
-                    (* Retrieving type of input. *)
-                    type_of input :: types,
-                    (* Creating term at 0. *)
-                    term_0_of input :: terms
-                ) ([], [])
-              in
-
-              undefined_outputs
-              |> List.fold_left (
-                fun (ufs, eqs) output ->
-                  let uf_name =
-                    Format.asprintf "%a.%s.%s"
-                    HString.pp_print_hstring (NI.get_internal_name node.node_id)
-                      (StateVar.name_of_state_var output)
-                      Lib.ReservedIds.function_of_inputs
-                  in
-                  let uf =
-                    UfSymbol.mk_uf_symbol
-                      uf_name input_types (type_of output)
-                  in
-                  uf :: ufs,
+            match comp_type with
+            | Function { uf_symbols } when options.add_functional_constraints -> (
+              let function_ufs = SVM.bindings uf_symbols |> List.map snd in
+              let constraints =
+                let term_0_of svar =
+                  Var.mk_state_var_instance svar Numeral.zero
+                  |> Term.mk_var
+                in
+                let input_terms_at_0 =
+                  let inputs = D.values inputs in
+                  List.map (fun input -> term_0_of input) inputs
+                in
+                undefined_outputs
+                |> List.map (fun output ->
+                  let uf = SVM.find output uf_symbols in
                   Term.mk_eq [
                     term_0_of output ;
                     Term.mk_uf uf input_terms_at_0
-                  ] :: eqs
-              ) ([], [])
+                  ]
+                )
+              in
+              function_ufs, constraints
             )
+            | _ -> [], []
           in
 
 

src/lustre/lustreNode.ml
@@ -143,6 +143,14 @@ type equation = equation_lhs * E.t
 (* A contract. *)
 type contract = C.t
 
+type func_info = {
+  uf_symbols : UfSymbol.t StateVar.StateVarMap.t
+}
+
+type type_of_component =
+  | Node
+  | Function of func_info
+
 (* A Lustre node *)
 type t = { 
 
@@ -197,8 +205,8 @@ type t = {
   (* Node is annotated as main node *)
   is_main : bool ;
 
-  (* Node is actually a function. *)
-  is_function: bool ;
+  (* Type of the component and its associated info *)
+  comp_type : type_of_component;
 
   (* Map from a state variable to its source *)
   state_var_source_map : state_var_source SVM.t;
@@ -211,41 +219,6 @@ type t = {
 
   history_svars: (StateVar.t * StateVar.t) list TM.t;
 }
-
-(* An empty node *)
-let empty_node (node_id: NI.t) is_extern = {
-  node_id ;
-  is_extern ;
-  opacity = Translucent;
-  instance = 
-    StateVar.mk_state_var
-      ~is_const:true
-      (I.instance_ident |> I.string_of_ident false)
-      (I.to_scope (NI.get_internal_name node_id |> I.of_hstring) @ I.reserved_scope)
-      Type.t_int;
-  init_flag = 
-    StateVar.mk_state_var
-      (I.init_flag_ident |> I.string_of_ident false)
-      (I.to_scope (NI.get_internal_name node_id |> I.of_hstring) @ I.reserved_scope)
-      Type.t_bool;
-  inputs = D.empty;
-  oracles = [];
-  outputs = D.empty;
-  locals = [];
-  equations = [];
-  calls = [];
-  asserts = [];
-  props = [];
-  contract = None ;
-  is_main = false;
-  is_function = false ;
-  state_var_source_map = SVM.empty;
-  oracle_state_var_map = SVT.create 17;
-  state_var_expr_map = SVT.create 17;
-  assumption_svars = SVS.empty;
-  history_svars = TM.empty;
-}
-
 
 (* Pretty-print array bounds of index *)
 let pp_print_array_dims safe ppf idx = 
@@ -595,10 +568,13 @@ let pp_print_node_signature fmt { inputs ; outputs } =
        (function ([], e) -> ([], e) | (_ :: tl, e) -> (tl, e))
        (D.bindings outputs))
 
-
+let is_function { comp_type } =
+  match comp_type with
+  | Node -> false
+  | Function _ -> true
 
 (* Pretty-print a node *)
-let pp_print_node safe ppf {
+let pp_print_node safe ppf ({
   node_id;
   inputs; 
   oracles; 
@@ -610,8 +586,7 @@ let pp_print_node safe ppf {
   props;
   contract;
   is_main ;
-  is_function ;
-} =
+} as node) =
 
   (* Output a space if list is not empty *)
   let space_if_nonempty = function
@@ -633,7 +608,7 @@ let pp_print_node safe ppf {
      tel;@]@]@?"  
 
     (* %s *)
-    (if is_function then "function" else "node")
+    (if is_function node then "function" else "node")
 
     (* %a *)
     NI.pp_print_node_id_user_name node_id
@@ -752,7 +727,7 @@ let pp_print_node_debug ppf
       props;
       contract;
       is_main;
-      is_function;
+      comp_type;
       state_var_source_map;
       oracle_state_var_map;
       state_var_expr_map; } = 
@@ -814,6 +789,11 @@ let pp_print_node_debug ppf
         Property.pp_print_prop_source source
   in
 
+  let pp_print_comp_type ppf = function
+    | Node -> Format.fprintf ppf "Node"
+    | Function _ -> Format.fprintf ppf "Function"
+  in
+
   let pp_print_state_var_source ppf = 
     let p sv src = Format.fprintf ppf "%a:%s" StateVar.pp_print_state_var sv src in
     function 
@@ -859,7 +839,7 @@ let pp_print_node_debug ppf
          props =      [@[<hv>%a@]];@ \
          contract =   [@[<hv>%a@]];@ \
          is_main =    @[<hv>%B@];@ \
-         is_function =    @[<hv>%B@];@ \
+         comp_type =    @[<hv>%a@];@ \
          state_var_source_map = [@[<hv>%a@]];@ \
          oracle_state_var_map = [@[<hv>%a@]];@ \
          state_var_expr_map = [@[<hv>%a@]]; }@]"
@@ -881,7 +861,7 @@ let pp_print_node_debug ppf
         Format.fprintf fmt "%a@ "
           (C.pp_print_contract_debug false) contract) contract
     is_main
-    is_function
+    pp_print_comp_type comp_type
     (pp_print_list pp_print_state_var_source ";@ ")
       (SVM.bindings state_var_source_map)
     (pp_print_list pp_print_oracle_state_var_map ";@ ")

src/lustre/lustreNode.mli
@@ -145,6 +145,14 @@ type equation_lhs = StateVar.t * LustreExpr.expr LustreExpr.bound_or_fixed list
     state variable as an expression. *)
 type equation = equation_lhs * LustreExpr.t
 
+type func_info = {
+  uf_symbols : UfSymbol.t StateVar.StateVarMap.t
+}
+
+type type_of_component =
+  | Node
+  | Function of func_info
+
 (** A Lustre node
 
     Every state variable occurs exactly once in {!t.inputs},
@@ -220,8 +228,8 @@ type t = {
   is_main : bool;
   (** Flag node as the top node *)
 
-  is_function : bool;
-  (** Node is actually a function *)
+  comp_type : type_of_component;
+  (* Type of the component and its associated info *)
 
   state_var_source_map : state_var_source StateVar.StateVarMap.t;
   (** Map from a state variable to its source 
@@ -257,11 +265,6 @@ type state_var_def =
   | IfBlock of position
   | ContractItem of position * LustreContract.svar * contract_item_type
   | Assertion of position
-
-(** Return a node of the given name and is extern flag without inputs, outputs,
-    oracles, equations, etc. Create a state variable for the {!t.instance} and
-    {!t.init_flag} fields, and set {!t.is_main} to false. *)
-val empty_node : NI.t -> bool -> t
 
 (** {1 Pretty-printers} *)
 
@@ -339,6 +342,9 @@ val stateful_vars_of_node : LG.state_var_bounds -> t -> StateVar.StateVarSet.t
 
 (** Return the name of the node *)
 val node_id_of_node : t -> NI.t
+
+(** Return whether the component is a function *)
+val is_function : t -> bool
 
 (** [ordered_equations_of_node n stateful init]
     Returns the equations of [n], topologically sorted by their base (step)

src/lustre/lustreNodeGen.ml
@@ -624,6 +624,26 @@ let compile_contract_item map count scope kind pos name expr =
     N.add_state_var_def state_var (N.ContractItem (pos, contract_sv, kind));
     contract_sv
 
+let create_uf_symbols node_id inputs undefined_outputs =
+  let type_of = StateVar.type_of_state_var in
+  let input_types = List.map type_of (X.values inputs) in
+
+  undefined_outputs
+  |> List.fold_left (
+    fun uf_symbols output ->
+      let uf_name =
+        Format.asprintf "%a.%s.%s"
+        HString.pp_print_hstring (NI.get_internal_name node_id)
+          (StateVar.name_of_state_var output)
+          Lib.ReservedIds.function_of_inputs
+      in
+      let uf =
+        UfSymbol.mk_uf_symbol
+          uf_name input_types (type_of output)
+      in
+      SVM.add output uf uf_symbols
+  ) SVM.empty
+
 let rec compile ctx gids decls =
   let over_decls cstate decl = compile_declaration cstate gids ctx decl in
   let output = List.fold_left over_decls (empty_compiler_state ()) decls in 
@@ -1790,10 +1810,11 @@ and compile_node_decl gids_map is_function opac cstate ctx node_id ext params in
       | _ -> assert false (* Guaranteed by LustreSyntaxChecks *)
     and is_single = List.length outputs = 1
     in List.fold_left (over_outputs is_single) X.empty outputs
+  in
   (* ****************************************************************** *)
   (* User Locals                                                        *)
   (* ****************************************************************** *)
-  in let locals, cstate =
+  let locals, cstate =
     let over_locals = fun (locals, cstate) local ->
       match local with
       | A.NodeVarDecl (_, (_, i, ast_type, A.ClockTrue)) ->
@@ -2739,6 +2760,24 @@ and compile_node_decl gids_map is_function opac cstate ctx node_id ext params in
       | None -> ()
   );
   (* ****************************************************************** *)
+  (* Type of component                                                  *)
+  (* ****************************************************************** *)
+  let comp_type =
+    if is_function then
+      let undefined_outputs =
+        let defined_svars = List.fold_left
+          (fun set ((sv,_),_) -> SVS.add sv set) SVS.empty equations
+        in
+        let is_undefined svar = SVS.mem svar defined_svars |> not in
+        List.filter is_undefined (X.values outputs)
+      in
+      N.Function { uf_symbols = 
+        create_uf_symbols node_id inputs undefined_outputs
+      }
+    else
+      N.Node
+  in
+  (* ****************************************************************** *)
   (* Finalize and build intermediate LustreNode                         *)
   (* ****************************************************************** *)    
   let locals = sofar_local @ ghost_locals @ glocals @ locals in
@@ -2787,7 +2826,7 @@ and compile_node_decl gids_map is_function opac cstate ctx node_id ext params in
     props;
     contract;
     is_main;
-    is_function;
+    comp_type;
     state_var_source_map;
     oracle_state_var_map;
     state_var_expr_map;

src/lustre/lustrePath.ml
@@ -1108,8 +1108,7 @@ let rec pp_print_lustre_path_pt' ?(full_contract=false) is_top const_map const_f
 (* Take first node to print *)
 | (
    trace, Node (
-    { N.node_id; N.inputs; N.outputs; N.locals;
-      N.is_function; } as node,
+    { N.node_id; N.inputs; N.outputs; N.locals } as node,
     model, required_modes, ensured_modes, contract_assumptions, contract_guarantees, call_conds, subnodes
   )
 ) :: tl when N.node_is_visible node ->
@@ -1123,7 +1122,7 @@ let rec pp_print_lustre_path_pt' ?(full_contract=false) is_top const_map const_f
 
   let node_name = NI.get_user_name node_id |> HString.string_of_hstring in
   let title =
-    if is_function then "Function"
+    if N.is_function node then "Function"
     else match (NI.get_node_type node_id) with 
     | Environment -> "Environment of"
     | Contract -> "Contract of"
@@ -1513,7 +1512,7 @@ let rec pp_print_lustre_path_xml' is_top const_map const_funcs ppf = function
 
   | (
     trace, Node (
-      { N.node_id; N.inputs; N.outputs; N.locals; N.is_function } as node,
+      { N.node_id; N.inputs; N.outputs; N.locals } as node,
       model, required_modes, ensured_modes, contract_assumptions, contract_guarantees , call_conds, subnodes
     )
   ) :: tl when N.node_is_visible node ->
@@ -1528,7 +1527,7 @@ let rec pp_print_lustre_path_xml' is_top const_map const_funcs ppf = function
     let name = NI.get_user_name node_id |> HString.string_of_hstring in
 
     let title =
-      if is_function then "Function"
+      if N.is_function node then "Function"
       else "Node"
     in
 
@@ -2005,7 +2004,7 @@ let rec pp_print_lustre_path_json' is_top const_map const_funcs ppf = function
   | [] -> ()
 
   | (
-    trace, Node ({ N.node_id; N.is_function } as node,
+    trace, Node ({ N.node_id } as node,
       model, required_modes, ensured_modes, contract_assumptions, contract_guarantees, call_conds, subnodes
     )
   ) :: tl when N.node_is_visible node ->
@@ -2018,7 +2017,7 @@ let rec pp_print_lustre_path_json' is_top const_map const_funcs ppf = function
     let name = NI.get_user_name node_id |> HString.string_of_hstring in
 
     let title =
-      if is_function then "function"
+      if N.is_function node then "function"
       else "node"
     in
 

src/lustre/lustreSlicing.ml
@@ -551,7 +551,7 @@ let slice_all_of_node
       N.props; 
       N.contract;
       N.is_main;
-      N.is_function;
+      N.comp_type;
       N.state_var_source_map;
       N.oracle_state_var_map;
       N.state_var_expr_map;
@@ -577,7 +577,7 @@ let slice_all_of_node
     N.props = if keep_props then props else [];
     N.contract = if keep_contracts then contract else None;
     N.is_main;
-    N.is_function;
+    N.comp_type;
     N.state_var_source_map = state_var_source_map;
     N.oracle_state_var_map = oracle_state_var_map;
     N.state_var_expr_map = state_var_expr_map;

src/realizability.ml
@@ -637,8 +637,8 @@ let compute_deadlocking_trace_and_conflict
     let scope = (Analysis.info_of_param param).top in
     match InputSystem.get_lustre_node in_sys scope with
     | None -> sys, false
-    | Some { LustreNode.is_function } ->
-      if is_function then (
+    | Some node ->
+      if LustreNode.is_function node then (
         (* Recompute transition system adding functional constraints *)
         let sys, _ =
           InputSystem.trans_sys_of_analysis

commit 0fc3b164993cfcfe37739311a1bb9dbcf4df1de1

Use undefined var list AFTER slicing when defining UF for function (#1308)
Fixes a bug introduced in PR #1286

src/lustre/lustreTransSys.ml
@@ -2219,7 +2219,13 @@ let rec trans_sys_of_node' options globals top_name analysis_param
           let function_ufs, function_constraints_at_0 =
             match comp_type with
             | Function { uf_symbols } when options.add_functional_constraints -> (
-              let function_ufs = SVM.bindings uf_symbols |> List.map snd in
+              let function_ufs =
+                undefined_outputs |> List.map (fun sv ->
+                  match SVM.find_opt sv uf_symbols with
+                  | Some uf -> uf
+                  | None -> assert false
+                )
+              in
               let constraints =
                 let term_0_of svar =
                   Var.mk_state_var_instance svar Numeral.zero

tests/regression/success/undefined-array-in-function.lus
@@ -1,0 +1,6 @@
+const M: int;
+
+function N() returns (A: int^M);
+let
+  check true;
+tel