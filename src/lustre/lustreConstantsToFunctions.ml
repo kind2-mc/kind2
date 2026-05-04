5d1f91cd0b2036ff82df367ddf4539195077d057
Rob Lorch <lorchrob@gmail.com>
2026-05-04 17:32:29 -0400

    Datatypes: lustreConstantsToFunctions.ml

--- src/lustre/lustreConstantsToFunctions.ml
+++ src/lustre/lustreConstantsToFunctions.ml
@@ -206,7 +206,9 @@ let rec ty_contains_gids ctx ni ty =
   | A.AbstractType _ | A.EnumType _
   | A.Bool _ | A.Int _ | A.Real _ | A.SBitVector _ | A.UBitVector _
   | A.UserType _ -> false
-  | A.ADT _ -> failwith "ADTs not yet implemented"
+  | A.ADT (_, _, cons) ->
+    let tys = List.concat_map (fun (_, tys) -> tys) cons in
+    List.fold_left (||) false (List.map r tys)
 
 (* Convert free constants to imported functions without args if there are (will be) associated 
    generated identifiers *)
