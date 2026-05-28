72e88e72d31d7f60866c6c7123f2c54e921c8c9e
Rob Lorch <lorchrob@gmail.com>
2026-05-28 13:37:37 -0400

    Allow multiple underscores in pattern

--- src/lustre/lustreSyntaxChecks.ml
+++ src/lustre/lustreSyntaxChecks.ml
@@ -1070,7 +1070,8 @@ and check_pattern_no_duplicates ctx pat =
   let rec check seen = function
     | [] -> Ok ()
     | (pos, id) :: rest ->
-      if List.mem id seen then syntax_error pos (DuplicatePatternVariable id)
+      if (List.mem id seen && not (id = HString.mk_hstring "_")) 
+      then syntax_error pos (DuplicatePatternVariable id)
       else check (id :: seen) rest
   in
   check [] (collect pat)
