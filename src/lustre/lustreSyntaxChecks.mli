82e1215e5ee79867e88f5563dec3ca9ef622bf52
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2026-06-08 12:36:15 -0500

    Handle lazy quantified call contexts

--- src/lustre/lustreSyntaxChecks.mli
+++ src/lustre/lustreSyntaxChecks.mli
@@ -34,6 +34,8 @@ type error_kind = Unknown of string
   | QuantifiedVariableInPre of HString.t
   | QuantifiedVariableInNodeArgument of HString.t * HString.t
   | SymbolicArrayIndexInNodeArgument of HString.t * HString.t
+  | QuantifiedVariableInLazyGuardedNodeCall of HString.t * HString.t
+  | SymbolicArrayIndexInLazyGuardedNodeCall of HString.t * HString.t
   | QuantifiedVariableInTypeAscription of HString.t 
   | SymbolicArrayIndexInTypeAscription of HString.t 
   | IllegalNodeCall of (HString.t * string)
