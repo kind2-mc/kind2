b6473e1f7bb0189a788957ee0411a7c7c738ffed
Josh <jbergthold@uiowa.edu>
2026-06-15 07:58:54 -0500

    Hardcode limit of 5

--- src/model.ml
+++ src/model.ml
@@ -143,7 +143,10 @@ let pp_print_map_as_array as_type ppf m =
       Format.fprintf ppf "[@[<hov 0>";
     done;
     let first = ref true in
+    let num_printed = ref 0 in
+    let over_printed = ref false in
     MIL.iter (fun l v ->
+        if !num_printed >= 5 then over_printed := true else (
         Array.blit current 0 prev 0 dim;
         Array.blit (Array.of_list l) 0 current 0 dim;
         let cpt = ref 0 in
@@ -165,8 +168,10 @@ let pp_print_map_as_array as_type ppf m =
         in
         Format.fprintf ppf "%*s%a"
           (val_width - w) "" (pp_print_value_term ty) v;
+        incr num_printed;
         first := false;
-      ) m;
+      )) m;
+    if !over_printed then Format.fprintf ppf ", ...";
     for _ = 1 to dim do
       Format.fprintf ppf "]@]";
     done
