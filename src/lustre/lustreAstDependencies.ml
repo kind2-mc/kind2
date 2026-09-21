8b6f8de14bd102b5dbf7afa01ed4ddf23939aad6
Rob Lorch <lorchrob@gmail.com>
2026-09-21 10:49:03 -0500

    Address PR comments

--- src/lustre/lustreAstDependencies.ml
+++ src/lustre/lustreAstDependencies.ml
@@ -1479,33 +1479,55 @@ let summarize_ip_vars: LA.ident list -> SI.t -> int list = fun ips critial_ips -
     then (nums::acc, nums+1)
     else (acc, nums+1)) ([], 0)) ips |> fst 
 (** Helper function to generate a node summary *)
-  
+
+(* Variables each left hand side of the node items depends on, with node calls
+   flattened through the summaries of the called nodes. The accumulated
+   condition variables come from the enclosing branching blocks: a condition
+   selects which definition applies, so what is defined inside depends on it. *)
+let rec node_item_deps: node_summary -> SI.t -> LA.node_item -> (LA.ident * SI.t) list
+  = fun s cond_vars -> function
+  | Body (Equation (_, LA.StructDef (_, lhss), e)) ->
+    List.mapi
+      (fun idx item ->
+        let lhs_var = SI.choose (LH.vars_of_struct_item item) in
+        (lhs_var, SI.union cond_vars (vars_with_flattened_nodes s idx e)))
+      lhss
+  | Body (Assert _) -> []
+  | IfBlock (_, cond, nis1, nis2)
+  | WhenBlock (_, cond, nis1, nis2) ->
+    let cond_vars = SI.union cond_vars (vars_with_flattened_nodes s 0 cond) in
+    List.concat_map (node_item_deps s cond_vars) (nis1 @ nis2)
+  | FrameBlock (_, _, nes, nis) ->
+    List.concat_map (node_item_deps s cond_vars)
+      (List.map (fun ne -> LA.Body ne) nes @ nis)
+  | AnnotMain _ | AnnotProperty _ | Auto _ -> []
+
 let mk_node_summary: bool -> node_summary -> LA.node_decl -> bool -> node_summary
     = fun connect_imported s (i, imported, _, _, ips, ops, _, items, _) is_rec ->
   if not imported && not is_rec
   then 
     let op_vars = List.map (fun o -> LH.extract_op_ty o |> fst) ops in
     let ip_vars = List.map (fun o -> LH.extract_ip_ty o |> fst) ips in
-    let node_equations = List.concat (List.map LH.extract_node_equation items) in
-    let process_one_eqn = fun (LA.StructDef (_, lhs), e) ->
-      let ms = List.mapi (fun idx item -> 
-          let lhs_var = SI.choose (LH.vars_of_struct_item item) in
-          let vars = vars_with_flattened_nodes s idx e in
-          IMap.singleton lhs_var (SI.elements vars))
-        lhs
-      in
-      List.fold_left (IMap.union (fun _ _ v2 -> Some v2)) IMap.empty ms
-    in
-    let node_equation_dependency_map = List.fold_left
-      (IMap.union (fun _ _ v2 -> Some v2)) IMap.empty
-      (List.map process_one_eqn node_equations)
+    (* A variable may be defined more than once (once per branch of a block),
+       in which case it depends on the variables of all of its definitions. *)
+    let node_equation_dependency_map =
+      List.fold_left
+        (fun m (lhs_var, vars) ->
+          IMap.update lhs_var
+            (function None -> Some vars | Some vars' -> Some (SI.union vars vars'))
+            m)
+        IMap.empty
+        (List.concat_map (node_item_deps s SI.empty) items)
     in
 
     Debug.parse "Node equation dependency map for node %a {\n %a \n}"
       NI.pp_print_node_id_user_name i
       (Lib.pp_print_list (Lib.pp_print_pair (LA.pp_print_ident) (Lib.pp_print_list (LA.pp_print_ident) ", ") "->") "\n")
-      (IMap.bindings node_equation_dependency_map);
-    let mk_g = fun (lhs, vars) ->  G.connect (List.fold_left G.union G.empty (List.map G.singleton vars)) lhs in
+      (List.map (fun (lhs, vars) -> (lhs, SI.elements vars))
+         (IMap.bindings node_equation_dependency_map));
+    let mk_g = fun (lhs, vars) ->
+      G.connect (SI.fold (fun v g -> G.union (G.singleton v) g) vars G.empty) lhs
+    in
     let g = List.fold_left G.union G.empty (List.map mk_g (IMap.bindings node_equation_dependency_map)) in
 
     Debug.parse "Node equation graph: %a" G.pp_print_graph g;
